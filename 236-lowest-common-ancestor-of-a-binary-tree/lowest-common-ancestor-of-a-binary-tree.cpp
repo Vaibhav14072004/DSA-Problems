9b9b9353e2b90835c2697fa59235108a1e4caee8
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-07-16 19:36:57 +0530

    Time: 15 ms (31.71%) | Memory: 19.7 MB (5.40%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,5 +1,61 @@
+// Approach 2 -> get path from (root to p) and (root to q)
+// and then find traverse both paths and find common point...
+
+// TC= O(N)   Sc= O(N)
+
+class Solution{
+public:
+    bool getPath(TreeNode* root, TreeNode* target, vector<TreeNode*> &path)
+    {
+        if(root== NULL){
+            return false;
+        }
+        
+        path.push_back(root);
+
+        if(root== target){
+            return true;
+        }
+
+        if(getPath(root->left,target,path) || getPath(root->right,target,path))
+        {
+            return true;
+        }
+
+        path.pop_back(); // backtracking..
+        return false;
+    }
+
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root== NULL){
+            return NULL;
+        }
+
+        vector<TreeNode*> path1, path2;
+         
+        getPath(root, p, path1);
+        getPath(root, q , path2);
+
+        int j= 0;
+        while(j < path1.size() && j < path2.size() && path1[j]== path2[j])
+        {
+            j++;
+        }
+
+       return path1[j-1];
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Optimal 
 // Tc= O(N)   Sc= O(1)
+
+/*
 
 class Solution {
 public:
@@ -25,3 +81,5 @@ public:
         return rightNode;
     }
 };
+
+*/
