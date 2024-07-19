90acaaf992c048f56907f9234b042ada9446f6ec
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-07-19 16:39:49 +0530

    Time: 107 ms (87.17%) | Memory: 101.1 MB (77.72%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,7 +1,47 @@
 // Ques ->> Next greater element
 
+// Approach 1 -> Using only one parameter in stack
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& arr) {
+        int n= arr.size();
+        vector<int> nge(n,0);
+
+        // for storing index of next greater element...
+        stack<int> st;
+
+        // Traverse array from end...
+        for(int i=n-1; i>=0; i--)
+        {
+      // pop out elements until it is smaller than curr.. bcoz we have to find next greater temp
+            while(!st.empty() && arr[st.top()] <= arr[i])
+            {
+                st.pop();
+            }
+           
+           // (Next Greater element Index- Curr index) will give no of elements <= curr element
+            if(!st.empty())
+            {
+                nge[i]= st.top()- i;
+            }
+
+            // else nge[i]= 0;  ->> No need, as we have already initialized with 0
+
+            st.push(i);
+         }
+     return nge;
+    }
+};
+
+
+
+
+
 // Approach 2 -> Using pair in stack
 // TC= O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -33,3 +73,6 @@ public:
         return nge;
     }
 };
+
+*/
+
