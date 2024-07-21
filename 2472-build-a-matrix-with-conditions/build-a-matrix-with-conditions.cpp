2bbc0c56d5d5a6551ee24d8802caee0dd8e50595
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-07-21 20:02:20 +0530

    Time: 96 ms (71.83%) | Memory: 63.9 MB (61.13%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -1,9 +1,104 @@
+// Approach 2 -> BFS
+
+// TC= O[ 2* (V+E)] + O(topRow.size()^2)
+
+class Solution{
+public:
+   vector<int> topoSort(vector<vector<int>> &vec, int k)
+   {
+        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(k+1, 0);
+
+        // create adj list from adj matrix..
+        for(vector<int> &it: vec)
+        {
+            int u= it[0];
+            int v= it[1];
+            adj[u].push_back(v);  // create directed graph
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        vector<int> topo;
+        int cnt= 0;
+
+        for(int i=1; i<=k; i++)
+        {
+            if(indegree[i]== 0)
+            {
+                q.push(i);
+                cnt++;
+            }
+        }
+
+        while(!q.empty())
+        {
+            int node= q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto neighbour : adj[node])
+            {
+                indegree[neighbour]--;
+                if(indegree[neighbour]== 0)
+                {
+                    q.push(neighbour);
+                    cnt++;
+                }
+            }
+        }
+        
+        // If cycle is present...
+        if(cnt != k)
+        {
+            return {};
+        }
+
+    return topo;
+   }
+
+  
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& row, vector<vector<int>>& col)
+    {  
+        vector<int> topoRow= topoSort(row,k);
+        vector<int> topoCol= topoSort(col,k);
+
+        vector<vector<int>> ans(k, vector<int> (k, 0));
+
+        // Topological sort valid for DAG(Directed Acyclic Graph)
+        // if graph contains cycle... we cannot make matrix..
+
+        if(topoRow.empty() || topoCol.empty()){
+            return {};
+        }
+
+        for(int i=0; i<topoRow.size(); i++)
+        {
+            for(int j=0; j<topoCol.size(); j++)
+            {
+                  if(topoRow[i]== topoCol[j])
+                  {
+                      ans[i][j]= topoRow[i]; // topoCol[j];
+                  }
+            }
+        }
+     return ans;
+    }
+};
+
+
+
+
+
+
 // Ques of Topological Sort
 
-// Approach 1 - DFS
+// Approach 1 - DFS -> Beats 27.61 % users 
 // TC= O(V + E) > For directed graph topological sort
 
 // Overall TC= O [2*(V+E)] + O(topoRow.size()^2)
+
+/*
 
 class Solution {
 public:
@@ -101,3 +196,6 @@ public:
     return ans;
     }
 };
+
+
+*/
