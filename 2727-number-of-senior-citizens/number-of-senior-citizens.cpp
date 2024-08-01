92b577f5221fd34b5af81308fe6e835ab68fc8bb
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-08-01 09:50:36 +0530

    Time: 6 ms (60.14%) | Memory: 17.6 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -1,5 +1,34 @@
+// Approach 2
+// TC= O(N)
+
+class Solution{
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt= 0;
+
+        for(int i=0; i<details.size(); i++)
+        {
+            // subtracting - '0' converts it to str to integer...
+            int age= (details[i][11]- '0')* 10 + (details[i][12]- '0');
+            if(age > 60)
+            {
+                cnt++;
+            }
+        }
+     return cnt;
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> USing substr method..
 // TC= O(N)
 // stoi is used to convert string into integer...
+
+/*
 
 class Solution {
 public:
@@ -8,7 +37,7 @@ public:
 
         for(int i=0; i<details.size(); i++)
         {
-            // substr(Index, length)
+            // substr(Start Index, length)
             int age= stoi(details[i].substr(11,2));
             
             if(age > 60)
@@ -19,3 +48,5 @@ public:
         return cnt;
     }
 };
+
+*/
