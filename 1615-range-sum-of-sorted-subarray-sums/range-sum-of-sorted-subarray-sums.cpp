004a6ca2d0a212a657c6713cdcba7438c78b1217
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-08-04 15:46:08 +0530

    Time: 245 ms (8.09%) | Memory: 27.5 MB (17.30%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,5 +1,67 @@
+// Approach 2 -> Using Min heap (Priority_queue)
+// TC= O(N* log N) + O[(n*(n+1)/2 * log (n*(n+1)/2)] + O(N)
+// TC= O(N* log N) + O[N^2 log N^2] + O(N)
+
+// TC= O(N* log N) + O(N^2 log N) + O(N)
+
+// SC= O((n* (n+1))/2 =>>  O(N^2)
+
+
+class Solution{
+public:
+    #define MOD 1000000007
+
+    int rangeSum(vector<int>& nums, int n, int left, int right)
+    {
+        // {subarray Sum, END Index}
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        vector<int> subarraySum;
+        for(int i=0; i<n; i++)
+        {
+            pq.push({nums[i],i});
+        }
+        
+        // this will run for all subarrays... (n* (n+1))/2 
+        // all subarrays will be by default arranged in ascending order..
+        while(!pq.empty())
+        {
+            int currSum= pq.top().first;
+            int endIndex= pq.top().second;
+            pq.pop();
+            subarraySum.push_back(currSum);
+
+            if(endIndex+1 < n)
+            {
+               pq.push({currSum + nums[endIndex+1], endIndex+1});
+            }
+        }
+        
+        // 1 based indexing...
+        int ans= 0;
+        for(int i=left-1; i<right; i++)
+        {
+            ans= (ans + subarraySum[i]) % MOD;
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
 // BRUTE FORCE Approach ->> Generating all subarrrays...
-// TC= O(N^2) + O(N log N)
+// TC= O(N^2) + O[(n*(n+1)/2 * log (n*(n+1)/2)] + O(N)
+// TC= O(N^2) + O[N^2 log N^2] 
+
+// O(N^2) + O(N^2 log N)
+// SC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -30,3 +92,5 @@ public:
     return static_cast<int> (ans);
     }
 };
+
+*/
