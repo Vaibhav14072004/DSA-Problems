f133c6b115cab311ab4ea3ef82ffe85892c3bad7
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-08-15 17:35:27 +0530

    Time: 127 ms (89.41%) | Memory: 132.1 MB (5.19%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -2,9 +2,51 @@
 // mark all multiples of 2, 3, 5 .. to N ..as 0 as they are NOT prime
 
 
+// Approach 2 -> Optimized
+
+// TC= O(N* log log N) + O(N)
+
+// TC= O(N log log N)
+
+class Solution{
+public:
+    int countPrimes(int n)
+    {
+         vector<int> isPrime(n, 1);
+        
+         for(int i=2; i*i < n; i++)
+         {
+            if(isPrime[i]== 1)
+            {
+                // directly start from i*i as all before it has factors (i*1, i*2, i*3 ...already been marked before..
+                // it is a harmonic series ->> So TC= O(log log N)
+                for(int j= i*i; j<n; j+= i)
+                {
+                    isPrime[j]= 0;
+                }
+            }
+         }
+         
+         int cnt= 0;
+         for(int i=2; i<n; i++)
+         {
+            if(isPrime[i]== 1)
+            {
+                cnt++;
+            }
+         }
+       return cnt;  
+    }
+};
+
+
+
+
+
 // Approach 1 -> Without optimization...
 // TC= O(N* log N) + O(N) ->> O(N log N)
 
+/*
 
 class Solution{
  public:
@@ -36,7 +78,7 @@ class Solution{
     }
 };
 
-
+*/
 
 
 
