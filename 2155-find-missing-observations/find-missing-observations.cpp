b97168db3e479165a47974a0fb5b420ef324b67f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-05 22:45:40 +0530

    Time: 96 ms (63.48%) | Memory: 116.3 MB (86.03%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -1,3 +1,45 @@
+// TC= O(N+ M)
+
+// N ->> missing values...
+// M ->> given values...
+
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n){
+        int m= rolls.size();
+
+        long long sum= 0;
+        for(int i=0; i<m; i++)
+        {
+            sum+= rolls[i];
+        }
+
+        int sumN= mean*(m+n) - sum;
+
+        if(sumN < n || sumN > n*6)
+        {
+            return {};
+        }
+    
+    // initilaize equally... 
+        vector<int> ans(n, sumN/n);
+    
+    // if remainder left.. then put 1 ...
+        int rem= sumN % n;
+        for(int i=0; i<rem; i++)
+        {
+           ans[i]++;  
+        }
+    
+    return ans;
+    }
+};
+
+
+
+
+/*
+
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
@@ -31,3 +73,5 @@ public:
         return res;
     }
 };
+
+*/
