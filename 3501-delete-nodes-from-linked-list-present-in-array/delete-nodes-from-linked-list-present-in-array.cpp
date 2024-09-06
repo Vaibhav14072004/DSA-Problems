4958672f77b2633f6332e05da27a9211726e72c5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-06 08:35:03 +0530

    Time: 439 ms (61.29%) | Memory: 262.8 MB (44.44%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,32 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// TC= O(N + N) -> O(N)
+
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-
+        
         ListNode* dummy= new ListNode(-1);
-         dummy->next= head;
-         ListNode* curr= head;
-         ListNode* prev= dummy;
+        dummy->next= head;
 
-         unordered_set<int> st(nums.begin(),nums.end());
+        ListNode* curr= head;
+        ListNode* prev= dummy;
 
-         while(curr!= NULL)
-         {
-             if(st.find(curr->val) != st.end())
-             {
-                  prev->next= curr->next;
-                  ListNode* temp= curr;
-                  curr= curr->next;
-                  delete temp;
-             }
-             else
-             {
-                  prev= curr;
-                  curr= curr->next;
-             }
-         }
+        unordered_set<int> st(nums.begin(), nums.end());
 
-      ListNode* newHead= dummy->next;
-      delete dummy;
-      return newHead;
+        while(curr != NULL)
+        {
+            // delete curr node if it is present in the set
+            if(st.find(curr->val) != st.end())
+            {
+                prev->next= curr->next;
+                ListNode* temp= curr;
+                curr= curr->next;
+                delete temp;
+            }
+
+            else
+            {
+                prev= curr;
+                curr= curr->next;
+            }
+        }
+
+       ListNode* newHead= dummy->next;
+       delete dummy;
+       return newHead;
     }
 };
