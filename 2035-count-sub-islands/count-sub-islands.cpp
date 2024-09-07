e23f4547e9ceae60b6dfda6f65b500883f1630cf
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-07 11:16:53 +0530

    Time: 225 ms (40.79%) | Memory: 99.4 MB (44.66%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,3 +1,75 @@
+// Approach 1 -> Using DFS 
+// TC= O(N*M)
+
+class Solution {
+public:
+   bool dfs(int i, int j, vector<vector<int>> &visited, vector<vector<int>> &grid1, vector<vector<int>> &grid2)
+   {
+       int n= grid1.size();    // row
+       int m= grid1[0].size(); // col
+
+       visited[i][j]= 1;
+      
+      // do not perform dfs if grid1[i][j] != 1
+
+      bool isIsland= true;
+
+       if(grid1[i][j]== 0)
+       {
+          isIsland= false;
+       }
+       
+       // check for all 4 directions, up,down,left,right
+       int row[4]= {-1,0,1,0};
+       int col[4]= {0,1,0,-1};
+       
+       for(int k= 0; k<4; k++)
+       {
+          int newRow= i+ row[k];
+          int newCol= j+ col[k];
+          
+        if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !visited[newRow][newCol] && grid2[newRow][newCol]== 1)
+          {
+              if(!dfs(newRow, newCol,visited,grid1,grid2))
+              {
+                isIsland= false;
+              }
+          }
+       }
+      return isIsland; 
+   }
+
+
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2)
+    {
+        int n= grid1.size(); // rows
+        int m= grid1[0].size(); // columns
+
+        vector<vector<int>> visited(n, vector<int> (m, 0));
+        
+        int cnt= 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(!visited[i][j] && grid2[i][j]== 1)
+                {
+                    if(dfs(i,j,visited,grid1,grid2))
+                       cnt++;
+                }
+            }
+        }
+       return cnt; 
+    }
+};
+
+
+
+
+
+
+/*
+
 class Solution {
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
@@ -40,3 +112,5 @@ public:
 
     }
 };
+
+*/
