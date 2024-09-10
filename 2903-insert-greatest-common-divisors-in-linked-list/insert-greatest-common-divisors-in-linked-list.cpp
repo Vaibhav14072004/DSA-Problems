8f878cc3340a651e474523cd8abb7456b8056595
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-10 10:57:48 +0530

    Time: 39 ms (60.32%) | Memory: 35.6 MB (9.80%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -9,10 +9,34 @@
  * };
  */
 
+
+
+// Approach 1 -> Using stl function for gcd calculation...
 // TC= O[N* log(min(a,b)]
 
 class Solution {
 public:
+    int gcd1(int a, int b)
+    {
+        if(b== 0)
+        {
+            return a;
+        }
+       return gcd1(b, a % b); 
+    }
+
+    int gcd2(int a, int b)
+    {
+        while(b != 0)
+        {
+            int temp= b;
+            b= a % b;
+            a= temp;
+        }
+       return a; 
+    }
+
+
     ListNode* insertGreatestCommonDivisors(ListNode* head)
     {
         // base case..
@@ -27,7 +51,11 @@ public:
         while(curr != NULL)
         {
             // TC for gcd() is log(min(a,b))...
-            int gcd= __gcd(curr->val, prev->val);
+
+            // int gcd= __gcd(curr->val, prev->val);
+
+           // int gcd= gcd1(curr->val, prev->val);
+            int gcd= gcd2(curr->val, prev->val);
 
             ListNode* temp= new ListNode(gcd); 
             prev->next= temp;
