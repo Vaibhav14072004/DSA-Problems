a5c64b2be84d09ebf0f6ae487c4ebc83d2187295
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-13 15:01:59 +0530

    Time: 60 ms (61.22%) | Memory: 41.6 MB (84.53%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -0,0 +1,38 @@
+// Approach 2 -> Best Approach (Calculating prefix xor array)...
+// TC= O(M+N)
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries)
+    {
+        int n= arr.size();
+        vector<int> prefXOR(n);
+        prefXOR[0]= arr[0];
+
+        for(int i=1; i<n; i++)
+        {
+            prefXOR[i]= prefXOR[i-1]^arr[i];
+        }
+
+        int m= queries.size();
+        vector<int> ans(m);
+
+        for(int i=0; i<m; i++)
+        {
+            int left= queries[i][0];
+            int right= queries[i][1];
+           
+           // same terms on taking xor will cancel each other...
+           if(left== 0)
+           {
+              ans[i]= prefXOR[right];
+           }
+
+           else
+           {
+              ans[i]= prefXOR[left-1] ^ prefXOR[right];
+           }
+        }
+       return ans; 
+    }
+};
