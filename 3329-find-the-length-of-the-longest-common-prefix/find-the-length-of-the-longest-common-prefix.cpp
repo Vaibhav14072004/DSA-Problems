6ff95505a65d0f55abd8a7f84f99d7f22a7f995e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-25 22:51:48 +0530

    Time: 235 ms (80.31%) | Memory: 160.9 MB (54.76%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,4 +1,87 @@
+// Approach 2 -> Using Trie
+// TC= O(N*d) + O(M*d) where d -> No of digits in no [ d= log10(num) + 1]
+
+struct Trie
+{
+    Trie* children[10];
+    Trie(){
+    for(int i=0; i<10; i++){
+      children[i]= 0;
+    }}
+};
+
+
+class Solution {
+public:
+    void insert(int num, Trie* root){
+         string str= to_string(num);
+         for(char ch: str)
+         {
+            int index= ch-'0';
+            if(!root->children[index])
+            {
+                root->children[index]= new Trie();
+            }
+
+            // traverse forward...
+            root= root->children[index]; 
+         }
+    }
+    
+
+    int search(int num, Trie* root){
+        string str= to_string(num);
+        
+        int len= 0;
+        for(char ch: str)
+        {
+            int index= ch-'0';
+            if(root->children[index])
+            {
+               len++;
+            }
+            
+            // if that char not found, break..
+            else
+            {
+                break;
+            }
+
+            // traverse forward...
+            root= root->children[index];
+        }
+      return len;  
+    }
+
+
+
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        Trie* root= new Trie(); 
+        for(int i=0; i<arr1.size(); i++)
+        {
+            insert(arr1[i],root);
+        }
+        
+        int maxLen= 0;
+        for(int i=0; i<arr2.size(); i++)
+        {
+           maxLen= max(maxLen, search(arr2[i],root));
+        }
+      return maxLen;  
+    }
+};
+
+
+
+
+
+
+
+
+
+
 // Approach 1 -> Using set
+// TC= O(N*d) + O(M*d) where d -> digits of no -> [1 + log10(num)]
 
 /*
 Store all prefixes of every number of arr1 in set.....
@@ -7,7 +90,7 @@ Now run a loop on arr2 and find all prefixes of every number of arr2, if that ex
 **** To find no of digits of a number ->> [log10 (num) + 1]
 */
 
-// TC= O(N*d) + O(M*d) where d -> digits of no -> [1 + log10(num)]
+/*
 
 class Solution {
 public:
@@ -37,7 +120,7 @@ public:
                 val= val/10;
             }
             
-            // if this prefix found in set... update maxLen with (no of digits of this prefix) ->[log10 (n) + 1]
+       // if this prefix found in set... update maxLen with (no of digits of this prefix) ->[log10 (n) + 1]
             if(val > 0)
             {
                 maxLen= max(maxLen, static_cast<int>(log10 (val) + 1));
@@ -46,3 +129,5 @@ public:
     return maxLen;
     }
 };
+
+*/
