735975ddebeb665d4e88904218f0e796b4bada1e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-25 19:16:33 +0530

    Time: 533 ms (78.30%) | Memory: 719.1 MB (44.64%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -1,8 +1,88 @@
-// Approach -> Using Trie
+// Approach 2-> Using Trie with initialization
 
 // TC= O(N*d) where d -> avg length of each word..
 // SC= O(N*d)
 
+struct TrieNode{
+   int cnt;
+   TrieNode* children[26];
+   TrieNode(){
+      cnt= 0;
+      for(int i=0; i<26; i++){
+         children[i]= NULL;
+      }
+   }
+};
+
+class Solution {
+public:
+
+    void insert(string word, TrieNode* root)
+    {
+        for(char ch: word)
+         {
+            int index= ch-'a';
+            if(!root->children[index])
+            {
+                root->children[index]= new TrieNode();
+            }
+            
+            // move forward....
+            root= root->children[index];
+            root->cnt+= 1;
+         }
+    }
+
+    int getScores(string word, TrieNode* root)
+    {
+        int count= 0;
+        for(char ch: word)
+        {
+            int index= ch-'a';
+        
+            // move forward...
+            root= root->children[index];
+            count+= root->cnt;
+        }
+       return count; 
+    }
+
+
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int n= words.size();
+        
+        TrieNode* root= new TrieNode();
+        for(int i=0; i<n; i++)
+        {
+           insert(words[i],root);
+        }
+        
+        vector<int> ans(n);
+        for(int i=0; i<n; i++)
+        {
+           ans[i]= getScores(words[i],root);
+        }
+       return ans; 
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// Approach 1-> Using Trie with separate func for initialization
+
+// TC= O(N*d) where d -> avg length of each word..
+// SC= O(N*d)
+
+/*
 
 struct TrieNode{
    int cnt;
@@ -71,3 +151,5 @@ public:
        return ans; 
     }
 };
+
+*/
