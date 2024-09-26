d89cb4bc623f332e9b9f3d5d3cb3c44c675733cf
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-09-26 12:22:51 +0530

    Time: 209 ms (15.30%) | Memory: 47.8 MB (10.75%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,47 @@
+// Approach 1 -> Using ordered_map
+// TC= O(N log N)
+
+// NOTE ->> We CANNOT use unordered_map bcoz it doesn't maintain any specific order of elements, and you need the map to be sorted by the event start times so we use ordered_map (map)
+
+
+
+class MyCalendar {
+public:
+   // Ordered_map with TC= O(log n)
+    map<int,int> mp;
+
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end)
+    {
+        // start-> +1    end-> -1
+        mp[start]++; 
+        mp[end]--;
+
+        int sum= 0;
+        for(auto it: mp)
+        {
+            sum+= it.second;
+
+            // if there is overlapping sum would become 2 ...
+            if(sum > 1)
+            {
+              // we are not adding this event in our ans, but we have incremented decremented above, so neutralize it
+                mp[start]--;
+                mp[end]++;
+
+                return false;
+            }
+        }
+       return true; 
+    }
+};
+
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
