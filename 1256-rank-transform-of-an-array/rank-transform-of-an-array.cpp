9ed9bebe78dea1da0e1bcaaf978ac2442dedbde5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-02 10:16:22 +0530

    Time: 58 ms (84.22%) | Memory: 43.1 MB (59.66%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,6 +1,53 @@
-// Approach -> Using Priority_queue
+// Approach 2 -> Sorting + Hashing
+// TC= O(N log N) + O(N) + O(N)  ->>>> O(N log N)
+
+class Solution{
+  public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n= arr.size();
+
+        // base case
+        if(arr.size()== 0)
+        {
+            return {};
+        }
+        
+        // {element, index}
+        unordered_map<int,int> mp;
+        
+        vector<int> sortedArr= arr;
+        sort(sortedArr.begin(),sortedArr.end());
+        
+        mp[sortedArr[0]]= 1;
+        int cnt= 1;
+
+        for(int i=1; i<n; i++)
+        {
+           if(sortedArr[i]!= sortedArr[i-1])
+           { 
+              cnt++;
+           }
+           mp[sortedArr[i]]= cnt;
+        } 
+
+        for(int i=0; i<n; i++)
+        {
+            arr[i]= mp[arr[i]];
+        }
+        return arr;
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> Using Priority_queue
 // TC= O(N log N) + O(N) ->> O(n log n)
 // SC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -37,3 +84,5 @@ public:
      return ans;   
     }
 };
+
+*/
