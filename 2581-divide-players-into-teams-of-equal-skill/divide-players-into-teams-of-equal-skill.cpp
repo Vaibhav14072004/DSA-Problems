6db9a9095f2b62aef88ba482b0a17e9030d14d39
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-04 19:40:52 +0530

    Time: 65 ms (53.00%) | Memory: 57.2 MB (14.60%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,49 +1,6 @@
-// Approach 2 -> Sorting
-// TC= O(N log N) + O(N) ->> O(n log n)
-
-class Solution {
-public:
-    long long dividePlayers(vector<int>& skill) {
-        int n= skill.size();
-
-        // base case -> if only 1 group can be formed
-        if(n== 2){
-            return skill[0]*skill[1];
-        }
-        
-        // sort in ascending order and map elements from first and last..
-        sort(skill.begin(), skill.end());
-
-        long long chemistry= 0;
-        int teamSize= skill[0]+ skill[n-1]; 
-        
-        int i=0, j= n-1;
-        while(i <= j)
-        {
-            int sum= skill[i]+ skill[j];
-            if(sum != teamSize){
-                return -1;
-            }
-
-           chemistry+= skill[i]* skill[j];
-           i++;
-           j--;
-        }
-    return chemistry;  
-    }
-};
-
-
-
-
-
-
-
-// Approach 1 -> Using unordered_map
+// Approach 1.1 -> Using unordered_map
 // TC= O(2* N) ->> O(N)
 // SC= O(N)
-
-/*
 
 class Solution {
 public:
@@ -61,10 +18,10 @@ public:
         
         long long teamSize= totalSum/(n/2);
 
-        // base case... if only one team can be formed
-        if(n == 2)
+        // base case... 
+        if(totalSum % (n/2) != 0)
         {
-           return skill[0]*skill[1];
+           return -1;
         }
 
         long long chemistry= 0;
@@ -106,6 +63,49 @@ public:
             }
         }
      return chemistry;
+    }
+};
+
+
+
+
+
+
+
+// Approach 2 -> Sorting
+// TC= O(N log N) + O(N) ->> O(n log n)
+
+/*
+
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        int n= skill.size();
+
+        // base case..
+        if(n== 2){
+            return skill[0]*skill[1];
+        }
+        
+        // sort in ascending order and map elements from first and last..
+        sort(skill.begin(), skill.end());
+
+        long long chemistry= 0;
+        int teamSize= skill[0]+ skill[n-1]; 
+        
+        int i=0, j= n-1;
+        while(i <= j)
+        {
+            int sum= skill[i]+ skill[j];
+            if(sum != teamSize){
+                return -1;
+            }
+
+           chemistry+= skill[i]* skill[j];
+           i++;
+           j--;
+        }
+    return chemistry;  
     }
 };
 
