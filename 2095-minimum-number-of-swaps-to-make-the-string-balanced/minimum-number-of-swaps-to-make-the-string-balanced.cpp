4313f8e22fed5189611b0b6e2153359e1097e845
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-08 17:31:39 +0530

    Time: 152 ms (9.09%) | Memory: 36.2 MB (12.86%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,10 +1,47 @@
-// Approach 1 -> Without stack
+// Approach 2 -> Using Stack (to cnt opening brackets)
+// TC= O(N)
+// SC= O(N)
+
+class Solution{
+  public:
+    int minSwaps(string str){
+        int n= str.length();
+        
+        stack<char> st;
+        for(int i=0; i<n; i++)
+        {
+            if(str[i]== '[')
+            {
+                st.push('[');
+            }
+
+            // if stack has open bracket -> '[' and str[i]== ']'
+            else if(!st.empty())
+            {
+                st.pop();
+            }
+        }
+
+        return (st.size()+1)/2;
+    }
+};
+
+
+
+
+
+
+
+// Approach 1 -> Without stack (to cnt opening bracket)
+
 // We need 1 swap for even no of opening bracket 
 // But if there is odd no of opening bracket, we need another 1 swap 
 // we are dividing by 2 because one swap is needed to solve 2 pairs 
 
 // TC= O(N)
 // SC= O(1)
+
+/*
 
 class Solution {
 public:
@@ -37,3 +74,5 @@ public:
         return (open+1)/2;
     }
 };
+
+*/
