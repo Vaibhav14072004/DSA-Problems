596a07af458f187fa651a70d6aceb1fc6699b61f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-19 23:43:19 +0530

    Time: 1 ms (54.99%) | Memory: 9 MB (42.93%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,35 +1,93 @@
+// If vector given hota, then we can directly access word, but in case of string, we use stringstream
+// In case of string, streamstream is used to access its words
+
+
+// Approach 2 -> Using stringstream + unordered_map
+// TC= O(3* N)
+
+class Solution{
+public:
+    vector<string> uncommonFromSentences(string str1, string str2) {
+        stringstream ss1(str1);
+        unordered_map<string,int> mp;
+        string word;
+
+        while(ss1 >> word)
+        {
+            mp[word]++;
+        }
+        
+        stringstream ss2(str2);
+        while(ss2 >> word)
+        {
+            mp[word]++;
+        }
+        
+        vector<string> ans;
+        for(auto it: mp)
+        {
+            if(it.second== 1)
+            {
+                ans.push_back(it.first);
+            }
+        }
+      return ans;  
+    }
+};
+
+
+
+
+
+// Approach 1 -> Using stringstream + 2 unordered_set
+// TC= O(3* N) ->> O(N)
+
+/*
+
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        vector<string> words;
-        stringstream ss(s1);
+        stringstream ss1(s1);
         string word;
-
-        unordered_map<string, int>flag;
-
-        while (ss >> word) {
-            if(flag[word]!=-1)
-                flag[word]++;
-            if(flag[word]>1)
-                flag[word]=-1;
-        }
         
+        unordered_set<string> st;
+        unordered_set<string> duplicates;
+        vector<string> ans;
+
+        while(ss1 >> word)
+        {
+            if(st.find(word) == st.end())
+            {
+                st.insert(word);
+            }
+            else
+            {
+                duplicates.insert(word);
+            }
+        }
+
         stringstream ss2(s2);
-        
-        while (ss2 >> word) {
-            if(flag[word]!=-1)
-                flag[word]++;
-            if(flag[word]>1)
-                flag[word]=-1;
+        while(ss2 >> word)
+        {
+            if(st.find(word) == st.end())
+            {
+                st.insert(word);
+            }
+            else
+            {
+                duplicates.insert(word);
+            }
         }
 
-        vector<string>res;
-
-        for(auto const&pair:flag) {
-            if(pair.second==1)
-                res.push_back(pair.first);
+        for(string it: st)
+        {
+            if(duplicates.find(it) == duplicates.end())
+            {
+                ans.push_back(it);
+            }
         }
-        
-        return res;
+    return ans; 
     }
 };
+
+*/
