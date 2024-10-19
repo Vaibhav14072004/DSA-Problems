9e048163675378ac004815aba66c3f726a96b122
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-19 13:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (54.37%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,10 +1,81 @@
-// Approach 1 -> Using priority_queue
-
+// <--- Priority_queue Intuition ->>>
 // We have to use the char that has max freq every time...
 // Considering the fact that no 3 consecutive 'a', 'b', 'c' come together
 
-// TC= O(a+b+c)
-// SC= O(a+b+c)
+
+
+// Approach 1.2 -> Priority_queue
+// TC= O(a+b+c)* log(3) ->> O(a+b+c)
+// SC= O(1)
+
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c)
+    {
+         // max heap...
+         priority_queue<pair<int,char>> pq;
+
+         if(a> 0) pq.push({a,'a'});
+         if(b >0) pq.push({b,'b'});
+         if(c> 0) pq.push({c,'c'});
+         
+         string ans= "";
+
+         while(!pq.empty())
+         {
+            int maxFreq= pq.top().first;
+            char ch= pq.top().second;
+            pq.pop();
+
+            // if we have already appended 'aa' or 'bb' or 'cc', use the secondmost frequent element
+            if(ans.size() >= 2 && ans.back()== ch && ans[ans.length()-2]== ch)
+            {
+                if(pq.empty())
+                {
+                    break;
+                }
+
+                int secondFreq= pq.top().first;
+                char secondCh= pq.top().second;
+                pq.pop();
+                
+                ans+= secondCh;
+                if(secondFreq-1 > 0)
+                {
+                    pq.push({secondFreq-1,secondCh});
+                }
+
+                // push the maxFreq charcater back after we have appended secondMost frequent element
+                pq.push({maxFreq,ch});
+            }
+
+            else
+            {
+                ans+= ch;
+                if(maxFreq-1 > 0)
+                {
+                    pq.push({maxFreq-1,ch});
+                }
+            }
+         } 
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+// Approach 1.1 -> Using priority_queue
+
+// TC= O(a+b+c)* log(3) ->> O(a+b+c)
+// SC= O(1)
+
+/*
 
 class Solution {
 public:
@@ -62,3 +133,5 @@ public:
      return ans;   
     }
 };
+
+*/
