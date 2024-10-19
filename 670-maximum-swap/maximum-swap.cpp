dd807ea67fe11a702fc966909f723f28fc77c4db
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-19 18:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (34.25%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,5 +1,51 @@
+// Approach 2 (Optimized) -> 
+
+// Take a vec of size- 9 to store the rightmost index of each char of string ..
+// For each char, traverse the vec of size- 9 from right each time, to swap with greatest element on its right
+
+// TC= O(N* 10) ->> O(N)
+// SC= O(10) ->> O(1)
+
+class Solution{
+public: 
+     int maximumSwap(int nums)
+     {
+        string str= to_string(nums);
+        int n= str.length();  
+        
+        // to store the index of each char of string
+        vector<int> vec(10,-1);
+        for(int i=0; i<n; i++)
+        {
+            vec[str[i]- '0']= i;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            int digit= str[i]-'0';
+            for(int j=9; j> digit; j--)
+            {
+                if(vec[j] > i)
+                {
+                    swap(str[i],str[vec[j]]);
+                    return stoi(str);
+                }
+            }
+        }
+    
+    return nums;
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Traverse from right and find store the idx of greatest element on right side
 // TC= O(N)
+
+/*
 
 class Solution{
  public:
@@ -11,10 +57,13 @@ class Solution{
         // to store the index of greatest element on right side
         vector<int> vec(n);
         vec[n-1]= n-1;
+        
 
         for(int i=n-2; i>=0; i--)
         {
-            // Store prev stored index in vec[i] 
+            // if we have not taken equal(=) sign then righmost greatest index will get replaced by leftmost index...
+            // For ex- 1993   output-  9913  but if we not take = sign then wrong out- 9193 which is incorrect
+
             if(str[i] <= str[vec[i+1]])
             {
                 vec[i]= vec[i+1];
@@ -41,3 +90,5 @@ class Solution{
     return stoi(str);    
    }     
 };
+
+*/
