e21eef55a82adc6cec187dbbe459ced19cde4fd3
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-19 15:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (89.55%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,20 +1,43 @@
-class Solution {
-public:
-    int maximumSwap(int num) {
-        string s = to_string(num);
-        vector<int> m ( 10 , -1);
-        for(int i =0;i<s.length();i++){
-            m[s[i]-'0'] = i;
-        }
-        string s2 = s;
-        sort(s2.rbegin() , s2.rend());
-        for(int i=0;i<s.length();i++){
-            if(s2[i] != s[i]){
-                int  it = m[s2[i]-'0'];
-                swap(s[i],s[it]);
-                break;
+// Approach 1 -> Traverse from right and find store the idx of greatest element on right side
+// TC= O(N)
+
+class Solution{
+ public:
+     int maximumSwap(int nums)
+     {
+        string str= to_string(nums);
+        int n= str.length();
+        
+        // to store the index of greatest element on right side
+        vector<int> vec(n);
+        vec[n-1]= n-1;
+
+        for(int i=n-2; i>=0; i--)
+        {
+            // Store prev stored index in vec[i] 
+            if(str[i] <= str[vec[i+1]])
+            {
+                vec[i]= vec[i+1];
+            }
+
+            // else the element itself is greatest ...
+            else
+            {
+                vec[i]= i;
             }
         }
-        return stoi(s);
-    }
+        
+        // Now do only 1 swap... if str[i] < str[vec[i]]
+        for(int i=0; i<n; i++)
+        {
+            if(str[i] < str[vec[i]])
+            {
+               swap(str[i],str[vec[i]]); 
+               break;
+            }
+        }
+    
+    // convert string back to integer...
+    return stoi(str);    
+   }     
 };
