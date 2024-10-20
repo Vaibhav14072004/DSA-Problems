abbb0b8fba6282f4c91470a798518e51a3315e0a
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-20 18:54:58 +0530

    Time: 317 ms (10.38%) | Memory: 140.6 MB (9.88%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,23 +1,90 @@
-class Solution {
+// Approach 1 -> Top Down Approach (Recursion + Memoization)
+// TC= O(N^2)
+
+class Solution{
 public:
-    int minExtraChar(string s, vector<string>& dictionary) {
-        int n = s.size();
-        unordered_set<string> dict(dictionary.begin(), dictionary.end());
-        vector<int> dp(n + 1, n); // dp[i] means the min extra chars from s[0..i-1]
-        dp[0] = 0;
+    int solve(int idx, string str, vector<string> &dict, unordered_set<string> &st, vector<int> &dp)
+    {
+        int n= str.length();
 
-        for (int i = 0; i < n; ++i) {
-            // Try to skip the current character
-            dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+         // base case
+         if(idx >= n){
+            return 0;
+         }
 
-            // Try to match a word in the dictionary
-            for (int j = i; j < n; ++j) {
-                if (dict.count(s.substr(i, j - i + 1))) {
-                    dp[j + 1] = min(dp[j + 1], dp[i]);
-                }
+         if(dp[idx] != -1){
+            return dp[idx];
+         }
+         
+         int mini= INT_MAX;
+         string curr= "";
+
+         for(int i= idx; i<n; i++){
+            curr+= str[i];
+            int extraChar= 0;
+
+            if(st.find(curr) == st.end())
+            {
+                extraChar= curr.length();
             }
+            
+            int rem= solve(i+1,str,dict,st,dp);
+            mini= min(mini, rem+ extraChar);
+         } 
+       return dp[idx]= mini;   
+    }
+
+
+    int minExtraChar(string str, vector<string>& dict){
+        int n= str.length();
+
+        unordered_set<string> st(dict.begin(),dict.end());
+        vector<int> dp(n+1,-1);
+        return solve(0,str,dict,st,dp);
+    }    
+};
+
+
+
+
+
+
+/* WRONG APPROACH -> Set, Map Cannot be used 
+
+class Solution{
+public:
+   int minExtraChar(string str, vector<string>& dictionary) {
+        unordered_map<char,int> mp;
+
+        for(int i=0; i<dictionary.size(); i++)
+        {
+             for(int j=0; j<dictionary[i].size(); j++)
+             {
+                mp[str[j]]++;
+             }
+        }
+        
+        int ans= 0;
+        for(int i=0; i<str.length(); i++)
+        {
+           mp[str[i]]--;
         }
 
-        return dp[n];
-    }
+        for(auto it: mp)
+        {
+            if(it.second > 0)
+            {
+                ans+= it.second;
+                it.second= 0
+            }
+        }
+    return ans/2;
+   }
 };
+
+*/
+
+
+
+
+
