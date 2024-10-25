a54334ea7d7a97147f66a334a9f63510767b6b5f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-25 15:13:28 +0530

    Time: 119 ms (82.14%) | Memory: 76.8 MB (43.13%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,5 +1,55 @@
+// Approach 2 -> Using unordered_set
+// TC= O(N^2)
+
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        int n= folder.size();
+        
+        unordered_set<string> st(folder.begin(), folder.end());
+        vector<string> ans;
+
+        for(int i=0; i<n; i++)
+        {
+            string str= folder[i];
+            bool flag= false;
+
+            // first find position of last '/'
+            for(int j=str.length()-1; j>=0; j--)
+            {
+                if(str[j]== '/')
+                {
+                    string curr= str.substr(0,j);
+
+                    // if front portion of curr string is found in set, it is subfolder
+                    if(st.find(curr) != st.end())
+                    {
+                        flag= true;
+                        break;
+                    }
+                }
+            }
+
+            if(!flag)
+            {
+                ans.push_back(folder[i]);
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
 // Approach 1 -> Using sorting
 // TC= O(N log N)
+
+/*
 
 class Solution {
 public:
@@ -26,3 +76,5 @@ public:
         return ans;
     }
 };
+
+*/
