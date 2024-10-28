6862a6243cc8dabe326af36c843efc2fc7fef823
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-28 16:14:40 +0530

    Time: 4 ms (69.12%) | Memory: 29.1 MB (26.95%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,5 +1,56 @@
+// Approach 2 -> Bottom UP (Tabulation)
+// TC= O(N*M)
+
+class Solution{
+public: 
+    int countSquares(vector<vector<int>>& matrix) {
+        int n= matrix.size();     // rows
+        int m= matrix[0].size();  // col
+
+        vector<vector<int>> dp(n, vector<int> (m,0));
+        
+        int ans= 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(matrix[i][j]== 1)
+                {
+                    // for filling the first row, first column
+                    if(i== 0 || j==0)
+                    {
+                        dp[i][j]= 1;
+                    }
+                    
+                    // in bottom up, we have filled in opp direction, (Up, Left, upDiagonal)
+                    // but in top down we have filed (Down, right, downDiagonal)
+                    else
+                    {
+                        dp[i][j]= 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1])); 
+                    }  
+
+                    ans+= dp[i][j];    
+                }
+            }
+        }
+
+    // It will only give squares ending at lastRow lastCol 
+    // return dp[n-1][m-1];
+
+     return ans;
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 -> Top Down (Recursion + Memoization)
 // TC= O(N*M)
+
+/*
 
 class Solution {
 public:
@@ -48,3 +99,5 @@ public:
     return ans;
     }
 };
+
+*/
