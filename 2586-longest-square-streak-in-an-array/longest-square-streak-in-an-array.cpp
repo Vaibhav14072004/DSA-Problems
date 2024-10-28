00a542c78b22b01d282e8a5286feb38fe0df0840
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-28 15:31:16 +0530

    Time: 70 ms (71.11%) | Memory: 86.3 MB (99.04%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,5 +1,75 @@
+// Approach 3 -> Binary Search
+// TC= O(N log N)
+
+class Solution{
+public:  
+    bool binarySearch(int val, vector<int> &nums)
+    {
+        int n= nums.size();
+        int start= 0;
+        int end= n-1;
+
+        while(start <= end)
+        {
+            int mid= start+ (end-start)/2;
+            if(val== nums[mid])
+            {
+                return true;
+            }
+            else if(val > nums[mid])
+            {
+                start= mid+1;
+            }
+            else
+            {
+                end= mid-1; 
+            }
+        }
+       return false; 
+    }
+
+
+    int longestSquareStreak(vector<int>& nums) {
+        int n= nums.size();
+
+        sort(nums.begin(),nums.end());
+        int maxLen= 0;
+
+        for(int i=0; i<n; i++)
+        {
+            long long curr= nums[i];
+            int len= 0;
+
+            // search whether nums[i]*nums[i] is present in array or not..
+            while(binarySearch(curr, nums))
+            {
+                len++;
+                curr= curr*curr;
+                
+                // bcoz constraint given value of element in array cannot exceed 10^5
+                if(curr > 100000)
+                {
+                    break;
+                }
+            }
+
+            maxLen= max(maxLen, len);
+        }
+    
+        if(maxLen < 2) return -1;
+
+        return maxLen;
+    }
+};
+
+
+
+
+
 // Approach 2 -> Sorting + unordered_map
 // TC= O(N log N)
+
+/*
 
 class Solution{
 public:
@@ -31,6 +101,10 @@ public:
     return maxLen;
     }
 };
+
+*/
+
+
 
 
 
