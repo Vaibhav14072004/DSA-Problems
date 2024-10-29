c84e480748c5b6c4f8a01309093321f82f836d1b
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-10-29 21:44:05 +0530

    Time: 54 ms (48.78%) | Memory: 195.7 MB (79.03%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -1,3 +1,97 @@
+// Aproach -> Using sorting
+// TC= O(N log N)
+
+class Solution {
+ public:
+     
+    /* If we write static bool cmp, then it will error bcoz it expects only 2 parameters,
+     use instead of it, we use lambda function directly inside sort()....
+
+    static bool cmp(int i,int j,vector<int> &positions)
+        return positions[i] < positions[j]; */
+
+
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n= positions.size();
+        vector<int> indexes(n);
+
+        for(int i=0; i<n; i++)
+        {
+            indexes[i]= i;
+        }
+
+        // sort indices based on ascending order of positions.. and write lambda func inside
+        sort(indexes.begin(),indexes.end(), [&](int a, int b)
+        {
+            return positions[a] < positions[b];
+        });
+        
+        stack<int> st;
+        for(int i=0; i<n; i++)
+        {
+            int idx= indexes[i];
+
+            // if it is moving in left direction..
+            if(directions[idx] == 'L')
+            {
+                   while(!st.empty() && directions[st.top()]== 'R')
+                   {
+                        if(healths[st.top()] > healths[idx])
+                        {
+                            healths[st.top()]--;
+                            healths[idx]= 0;
+                            break;
+                        }
+                        
+                        else if(healths[st.top()] < healths[idx])
+                        {
+                            healths[idx]--;
+                            healths[st.top()]= 0;
+                            st.pop();
+                        }   
+                        
+                        // if healths[st.top()]== healths[idx]
+                        else
+                        {
+                            healths[st.top()]= 0;
+                            healths[idx]= 0;
+                            st.pop();
+                            break;
+                        }  
+                   } 
+
+                   if(healths[idx] > 0)
+                   {
+                       st.push(idx);
+                   }
+            }
+
+            // if it is moving in 'R' direction, just push in stack
+            else
+            {
+                st.push(idx);
+            }
+        }
+        
+        vector<int> ans;
+        for(int i=0; i<n; i++)
+        {
+            if(healths[i] > 0)
+            {
+               ans.push_back(healths[i]);
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+/*
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions,
@@ -50,3 +144,5 @@ public:
         return result;
     }
 };
+
+*/
