57736feb8fd90125434e2424bc5dce75447751bd
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-02 11:26:14 +0530

    Time: 2 ms (14.55%) | Memory: 9.3 MB (12.39%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,7 +1,56 @@
-// Ques of stringstream
+// Ques of strigstream
 
+// Approach 1.2 -> Directly using vec.back() to access last char
 // TC= O(N)
 // SC= O(N)
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        stringstream ss(sentence);
+        string word;
+
+        vector<string> vec;
+        while(ss >> word)
+        { 
+           vec.push_back(word);
+        }
+        
+        int n=vec.size();
+
+        // base case -> if only one word, directly compare its first and last
+        if(n== 1)
+        {
+           return vec[0][0]== vec[0].back();
+        }
+
+        // compare bikul first to bilkul last character
+        if(vec[0][0] != vec[n-1].back())
+        {
+            return false;
+        }
+        
+        // now start comparing consecutive charcaters, first and last
+        for(int i=1; i<n; i++)
+        {
+            if(vec[i-1].back() != vec[i][0])
+            {
+                return false;
+            }
+        }
+    return true;
+    }
+};
+
+
+
+
+
+// Approach 1.1  -> Beats 14.55 % 
+// TC= O(N)
+// SC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -49,3 +98,5 @@ public:
         return true;
     }
 };
+
+*/
