b3bc42be81023eeea2c6364935602ed6c54b17f7
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-02 10:00:48 +0530

    Time: 2 ms (14.55%) | Memory: 9.3 MB (12.39%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,51 @@
+// Ques of stringstream
+
+// TC= O(N)
+// SC= O(N)
+
+class Solution {
+public:
+    bool isCircularSentence(string sentence){
+        stringstream ss(sentence);
+        string word;
+        
+        vector<string> vec;
+        while(ss >> word)
+        {
+            vec.push_back(word);
+        }
+        
+        int n= vec.size();
+
+        // base case ->> when only one word is present... check for its first and last char
+        if(n== 1)
+        {
+            return vec[0][0]== vec[0][vec[0].size()-1];
+        }
+
+        int lastLen= vec[n-1].size();
+        
+        // check for bilkul first and bilkul last character
+        if(n > 1 && vec[0][0] != vec[n-1][lastLen-1])
+        {
+            return false;
+        }
+        
+        // now check for consecutive words, first and last
+        int i=0,j=1;
+        while(i< j && j < n)
+        {
+            char last= vec[i][vec[i].size()-1];
+            char first= vec[j][0];
+
+            if(last != first)
+            {
+                return false;
+            }
+        i++;
+        j++;    
+        }
+
+        return true;
+    }
+};
