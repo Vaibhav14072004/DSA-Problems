c187adee08d93b45989e9da37152b322ba669110
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-03 12:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (83.69%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,5 +1,44 @@
+// Approach 2 -> Brute Force Approach
+// Generate all strings by shifting one character..
+
+// TC= O(N^2)
+
+class Solution{
+public:
+    bool rotateString(string str, string goal) {
+       if(str== goal)
+       {
+          return true;
+       }
+
+       if(str.length() != goal.length())
+       {
+          return false;
+       }
+
+       for(int i=1; i<= str.length(); i++)
+       {
+           // rotate func takes 3 parameters, middle parameter is the inex from which we have to rotate
+           rotate(str.begin(), str.begin()+1, str.end());
+           if(str== goal)
+           {
+              return true;
+           }
+        }
+          return false;  
+       }
+};
+
+
+
+
+
+
+
 // Approach 1 
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -22,3 +61,5 @@ public:
         return false;
     }
 };
+
+*/
