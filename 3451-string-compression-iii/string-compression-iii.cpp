5b72e43935edb343ae922ad0a04851963cdabd47
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-04 18:53:18 +0530

    Time: 63 ms (19.66%) | Memory: 29.9 MB (24.06%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -1,4 +1,71 @@
+// Approach 2 -> Using while loop
 // TC= O(N)
+
+class Solution {
+public:
+    string compressedString(string word) {
+        int n= word.length();
+
+        // base case
+        if(n== 0){
+         return "";
+        }
+
+        if(n==1){
+          string temp= to_string(1);
+          temp+= word[0];
+          return temp;
+        }
+
+        int i=0;
+        string ans= "";
+
+        while(i < n)
+        {
+            char curr= word[i];
+            int cnt= 1;
+            while(i+1 < n && curr== word[i+1])
+            {
+                cnt++;
+                i++;
+            }
+
+            if(cnt <= 9)
+            {
+                ans+= to_string(cnt);
+                ans+= curr;
+            }
+            else
+            {
+                int d= cnt/9;
+                int rem= cnt % 9;
+                while(d > 0)
+                {
+                    ans+= to_string(9);
+                    ans+= curr;
+                    d--;;
+                }
+
+                if(rem != 0)
+                {
+                    ans+= to_string(rem);
+                    ans+= curr;
+                }
+            }
+            i++;
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+// Approach 1 -> Using for loop
+// TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -35,3 +102,5 @@ public:
        return ans;  
     }
 };
+
+*/
