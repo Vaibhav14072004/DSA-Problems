9456c13f16aba25507a55d3e5a2dc380491249e5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-05 23:20:23 +0530

    Time: 1 ms (54.26%) | Memory: 12.2 MB (16.88%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,5 +1,51 @@
+// Approach 2 -> 
+
+class Solution {
+public:
+    int minChanges(string s) {
+        int n= s.length();
+        
+        int changes= 0;
+        int cnt= 0;
+
+        char prev= s[0];
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]== prev)
+            {
+                cnt++;
+            }
+            
+            // if s[i] != s[i-1]
+            else
+            {
+                // start a new group,
+                if(cnt % 2!= 0)
+                {
+                    changes++;
+                    cnt= 0;
+                }
+                else
+                {
+                    cnt= 1;
+                }
+
+                prev= s[i];
+            }
+        }
+    return changes;
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Best Approach
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -17,3 +63,5 @@ public:
       return changes;  
     }
 };
+
+*/
