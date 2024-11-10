351043d7aeb751374b64a22019fe5cfc37984cb8
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-10 19:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (8.06%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
@@ -1,5 +1,70 @@
+// Approach 2 -> Sliding Window Approach
+// TC= O(8* N)
+
+// Nums can go upto 50
+// No of bits needed= [1 + Upper bound[log 2 (50)]]= 7 bits
+// Instead of running loop till 32, run only upto 7
+
+class Solution {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int minLen = n+1;
+        
+        // to store cnt of set bits, Needed whe we have to shrink the 
+        // curr window, we need to remove nums[i] from currOR
+        vector<int> bits(32,0);
+
+        int i=0, j= 0, currOR= 0;
+        while(j < n)
+        {
+            currOR|= nums[j];
+            for(int idx= 0; idx<8; idx++)
+            {
+                // check if i th bit is set or NOT
+                if(nums[j] & (1<< idx))
+                {
+                    bits[idx]++;
+                }
+            }
+
+            // shrink the curr window..
+            while(i<= j && currOR >= k)
+            {
+                minLen= min(minLen, j-i+1);
+                for(int idx= 0; idx<8; idx++)
+                {
+                    if(nums[i] & (1<< idx))
+                    {
+                        bits[idx]--;
+                        if(bits[idx]== 0)
+                        {
+                            currOR-= (1<<idx);
+                        }
+                    }
+                }
+                i++;
+            }
+
+            j++;
+        }
+
+        if(minLen > n) return -1;
+
+        return minLen;
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 -> Brute Force Approach
 // TC= O(N^2)
+
+/*
 
 class Solution {
 public:
@@ -22,7 +87,9 @@ public:
         }
 
         if(minLen > n) return -1;
-        
+
         return minLen;
     }
 };
+
+*/
