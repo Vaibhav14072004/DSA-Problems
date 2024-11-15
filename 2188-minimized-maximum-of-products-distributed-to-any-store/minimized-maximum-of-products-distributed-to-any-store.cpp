a1da894f62977322279aa92a017f2ec9a16d8c4a
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-15 17:02:13 +0530

    Time: 91 ms (30.15%) | Memory: 87.2 MB (89.10%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,3 +1,61 @@
+// Approach 1 -> Binary Search
+// TC= O[M* log(maxElement)]
+
+
+class Solution{
+public:
+// Func to find no of partitions, if quantities[i] is divided into max of "mid" partitions
+    int partitions(vector<int> &quantities, int mid, int n)
+    {
+        int m= quantities.size();
+        int cnt= 0;
+        
+        for(int i=0; i<m; i++)
+        {
+            // ceil(a/b)=  (a+b-1)/b
+           cnt+= ceil((float)quantities[i]/mid);
+        }
+        return cnt;
+    }
+
+
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int m = quantities.size();
+        int maxi= *max_element(quantities.begin(),quantities.end());
+
+        // Now apply binary search between 1 to maxElement 
+        int start= 1;
+        int end= maxi;  
+        int ans= INT_MAX;
+
+        while(start<= end)
+        {
+            int mid= start+ (end-start)/2;
+            
+           // n should be greater than no of partitions formed if 
+           // quantities[i] is divided into max of "mid" partitions
+            if(n >= partitions(quantities,mid,n))
+            {
+                ans= mid;
+                end= mid-1;
+            }
+            else
+            {
+                start= mid+1;
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
+/*
 class Solution {
 public:
     bool possibleToDistribute(vector<int>& quantities, int shops, int x) {
@@ -11,7 +69,6 @@ public:
         }
         return true;
     }
-
     
     int minimizedMaximum(int n, vector<int>& quantities) {
         int m = quantities.size();
@@ -35,3 +92,5 @@ public:
         return result;
     }
 };
+
+*/
