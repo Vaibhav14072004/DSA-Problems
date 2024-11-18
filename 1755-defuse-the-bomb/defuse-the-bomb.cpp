1cae204e4d88172a620bbfbd899fb161757831ee
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-18 20:40:53 +0530

    Time: 1 ms (19.16%) | Memory: 10.4 MB (60.07%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,40 @@
+// Approach 1 -> Brute Force Approach
+// TC= O(N*k)
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n= code.size();
+        
+        vector<int> ans(n,0);
+        if(k== 0)
+        {
+            return ans;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            int sum= 0;
+
+            if(k> 0)
+            {
+                for(int j=i+1; j<= i+k; j++)
+                {
+                    sum+= code[j % n];
+                }
+                ans[i]= sum;
+            }
+            
+            // if(k < 0)
+            else
+            {
+                for(int j= i-1; j>= i+k; j--)
+                {
+                    sum+= code[(j+n) % n];
+                }
+                ans[i]= sum;
+            }
+        }
+        return ans;
+    }
+};
