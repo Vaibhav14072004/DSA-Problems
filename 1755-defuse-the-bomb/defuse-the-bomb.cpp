56c0cc01ed34f27d4b05292abb3f8de5ff748508
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-11-19 17:19:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.16%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,5 +1,60 @@
+// Approach 2 -> Sliding Window
+// TC= O(N)
+
+class Solution{
+public: 
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n= code.size();
+        
+        vector<int> ans(n,0);
+        if(k== 0)
+        {
+            return ans;
+        }
+        
+        int i=-1, j= -1;
+        int sum= 0;
+
+        if(k > 0)
+        {
+            i= 1;
+            j= k;
+        }
+        else
+        {
+            i= n- abs(k);  
+            j= n-1;
+        }
+        
+        // calculate the sum of the curr window of next 3 elements,starting from i to j
+        for(int idx= i; idx<= j; idx++)
+        {
+            sum+= code[idx % n];
+        }
+        
+        // now run a loop and remove the (i)th element and add (j+1) th element in sum
+        for(int idx= 0; idx<n; idx++)
+        {
+            ans[idx]= sum;
+            sum-= code[i % n];
+            i++;
+            sum+= code[(j+1) % n];
+            j++;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 -> Brute Force Approach
 // TC= O(N*k)
+
+/*
 
 class Solution {
 public:
@@ -38,3 +93,5 @@ public:
         return ans;
     }
 };
+
+*/
