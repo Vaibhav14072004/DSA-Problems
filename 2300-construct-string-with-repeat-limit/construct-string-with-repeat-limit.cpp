19dbfc6eba785256d897bc3d1173537f230ebf00
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-17 14:52:43 +0530

    Time: 20 ms (84.81%) | Memory: 28.1 MB (32.11%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,8 +1,77 @@
+// Approach 2 -> Using Max Heap
+// TC= O(N) + O(26) + O(26* (log26+ N)) ->> O(N)
+// SC= O(26)->> O(1)
+
+class Solution {
+public:
+    string repeatLimitedString(string str, int repeatLimit) {
+        int n= str.length();
+        vector<int> freq(26,0);
+
+        // max heap to give highest lexographically char that has freq>0
+        priority_queue<char> pq;
+
+        for(int i=0; i<n; i++)
+        {
+            freq[str[i]-'a']++;
+        }
+        
+        // put all characters in max heap having freq>0
+        for(int i=0; i<26; i++)
+        {
+            if(freq[i] > 0)
+            {
+                char ch= i+'a';
+                pq.push(ch);
+            }
+        }
+        
+        string ans= "";
+        while(!pq.empty())
+        {
+            char top= pq.top();
+            // push pop operations takes log(26)
+            pq.pop();
+
+            int cnt= min(freq[top-'a'],repeatLimit);
+
+            // appending takes O(N) time
+            ans.append(cnt,top);      
+            freq[top-'a']-= cnt;
+            
+            // if freq is still > 0, add prev char in between
+            if(freq[top-'a'] > 0 && !pq.empty())
+            {
+                int nextCh= pq.top();
+                pq.pop();
+
+                ans+= nextCh;
+                freq[nextCh-'a']--;
+                
+                if(freq[nextCh-'a'] > 0)
+                {
+                    pq.push(nextCh);
+                }
+               pq.push(top); 
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 -> Sliding Window
 // TC= O(N) + O(26 + N)  ->> O(N)
 // SC= O(26) -> O(1)
 
 // ans+=string(cnt,ch) or ans.append(cnt,ch) ->> Takes O(N) time
+
+/*
 
 class Solution {
 public:
@@ -51,3 +120,5 @@ public:
         return ans;
     }
 };
+
+*/
