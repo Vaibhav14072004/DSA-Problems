bb6556b3c83ced8662d771b546dd2e26ae007769
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-17 18:39:23 +0530

    Time: 7 ms (8.82%) | Memory: 27.8 MB (20.80%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,35 +1,28 @@
+// TC= O(K* log N)
+
 class Solution {
 public:
-    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier){
         int n= nums.size();
+        vector<int> ans(nums);
+        
+        // min heap...
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> pq;
 
-        // min heap (priority_queue)
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-        for(int i=0; i<n; i++)
-        {
-            pq.push({nums[i], i});
+        for(int i=0; i<n; i++){
+           pq.push({nums[i],i});
         }
 
-        while(k--)
+        while(!pq.empty() && k--)
         {
-            int top= pq.top().first;
-            int idx= pq.top().second;
+            int val= pq.top().first;
+            int index= pq.top().second;
             pq.pop();
 
-            pq.push({top*multiplier, idx});
-            nums[idx]= top*multiplier;
+            int newVal= multiplier*val;
+            ans[index]= newVal;
+            pq.push({newVal,index});
         }
-        
-        // vector<int> ans[n];
-        // while(!pq.empty())
-        // {
-        //    int index= pq.top().second;
-        //    int element= pq.top().first;
-        //    pq.pop();
-        //    ans[i]= 
-        // }
-    
-    return nums;
+        return ans;
     }
 };
