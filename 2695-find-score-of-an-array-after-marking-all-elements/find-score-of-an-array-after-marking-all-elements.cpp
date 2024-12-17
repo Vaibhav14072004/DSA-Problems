58a4cf3912551746f688100c9853652ad39ea861
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-17 17:44:25 +0530

    Time: 64 ms (71.05%) | Memory: 108.9 MB (32.36%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,7 +1,55 @@
-// Approach 1 -> Using min heap + visited vector
+// Approach 2 ->> Using sorting+ separate vec for storing pair +visited vec
+// TC= O(N log N) + O(2*N) ->> O(N log N)
+// SC= O(2*N)
 
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        long long score = 0;
+        vector<int> visited(n, 0);
+
+        vector<pair<int,int>> vec;
+        for(int i=0; i<n; i++){
+            vec.push_back({nums[i],i});
+        }
+
+        sort(vec.begin(),vec.end());
+
+        for(int i=0; i<n; i++)
+        {
+            int val= vec[i].first;
+            int index= vec[i].second;
+
+            if(visited[index]){
+                continue;
+            }
+
+            score+= val;
+            visited[index]= 1;
+
+            if(index-1 >= 0){
+                visited[index-1]= 1;
+            }
+
+            if(index+1 < n){
+                visited[index+1]= 1;
+            }
+        }
+      return score;
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> Using min heap + visited vector
 // TC= O(N) + O(N log N) =-> O(N log N)
 // SC= O(N+N) ->> O(N)
+
+/*
 
 class Solution {
 public:
@@ -22,8 +70,7 @@ public:
             int index = pq.top().second;
             pq.pop();
 
-            if(visited[index])
-            {
+            if(visited[index]){
                 continue;
             }
                 score += top;
@@ -37,7 +84,8 @@ public:
                     visited[index+1]= 1;
                 }       
         }
-
-        return score;
+      return score;
     }
 };
+
+*/
