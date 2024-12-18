2e88d4c78cbdae22148390e6051f5f0f252bae07
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-18 15:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (49.47%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,5 +1,59 @@
+// Approach 2 -> By storing valid index for every index
+// TC= O(2*N + Q) =>> O(N+Q)
+
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n= nums.size();
+        int q= queries.size();
+        vector<int> validEndIndex(n);
+
+        int i=0, j= 0;
+
+        // store valid end index for every index..
+        while(i < n)
+        {
+            if(j < i){
+               j= i;
+            }
+
+            while(j+1 < n && (nums[j] % 2 != nums[j+1] % 2)){
+                j++;
+            }
+
+            validEndIndex[i]= j;
+            i++;
+        }
+        
+        vector<bool> ans(q,false);
+        for(int i=0; i<q; i++)
+        {
+            int start= queries[i][0];
+            int end= queries[i][1];
+
+            if(end <= validEndIndex[start])
+            {
+                ans[i]= true;
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 // Approach 1 -> Using Prefix sum for how many invalid idx are there till now..
 // TC= O(N+Q)
+
+/*
 
 class Solution {
 public:
@@ -39,3 +93,5 @@ public:
         return ans;
     }
 };
+
+*/
