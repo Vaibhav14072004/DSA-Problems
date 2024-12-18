2fc838542dd46128c7c347825d16093ec869d939
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-18 17:29:40 +0530

    Time: 7 ms (54.10%) | Memory: 126.2 MB (93.91%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,5 +1,82 @@
+// Approach 3 ->> Storing invalid index + Binary Search
+// TC= O(Q* log N)
+
+class Solution {
+public:
+
+// binary Search func to check whether invalidIndex found in this range or NOT
+    bool invalidIndex_found(int startPt, int endPt,vector<int> &invalidIndex)
+    {
+        int start= 0;
+        int end= invalidIndex.size()-1;
+
+         while(start <= end)
+         {
+            int mid= start+ (end-start)/2;
+            if(invalidIndex[mid] < startPt)
+            {
+                start= mid+1;
+            }
+            else if(invalidIndex[mid] > endPt)
+            {
+               end= mid-1;
+            }
+            else
+            {
+                return true;
+            }
+         }
+      return false;
+    }
+
+
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n= nums.size();
+        int q= queries.size();
+        vector<bool> ans(q,true);
+        
+        vector<int> invalidIndex;
+        for(int i=1; i<n; i++)
+        {
+            if(nums[i-1] % 2 == nums[i] % 2)
+            {
+                invalidIndex.push_back(i);
+            }
+        }
+
+        // if no invalid index found..
+        if(invalidIndex.empty()){
+            return ans;
+        }
+
+        for(int i=0; i<q; i++)
+        {
+            int startPt= queries[i][0];
+            int endPt= queries[i][1];
+            
+            // apply binary Search to find whether any invalid index lies in this range (start+1 to end)
+            if(invalidIndex_found(startPt+1 ,endPt,invalidIndex))
+            {
+               ans[i]= false;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
 // Approach 2 -> By storing valid index for every index
 // TC= O(2*N + Q) =>> O(N+Q)
+
+/*
 
 class Solution {
 public:
@@ -40,6 +117,7 @@ public:
     }
 };
 
+*/
 
 
 
