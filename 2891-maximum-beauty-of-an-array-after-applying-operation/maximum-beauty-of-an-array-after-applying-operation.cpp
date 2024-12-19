6f372213e36211d7081e8228c031a44a1d0ae204
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-19 13:32:08 +0530

    Time: 67 ms (30.57%) | Memory: 119 MB (18.07%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,5 +1,56 @@
 // Approach 1 -> Find max no of overlapping intervals for each element(nums[i]-k, nums[i]+k)
+
+// Approach 1.2 ->> Using queue
+// NO need to take deque, no need to push start time in queue as we need only end time
+
 // TC= O(N* log N)+ O(2*N)
+
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n= nums.size();
+
+        vector<pair<int,int>> intervals(n);
+        for(int i=0; i<n; i++)
+        {
+             intervals[i]= {nums[i]-k, nums[i]+k};
+        }
+
+        sort(intervals.begin(),intervals.end());
+        
+        // We only need end time..
+        queue<int> q;
+
+        int maxSize= 0;
+        for(auto &it: intervals)
+        {
+            // Pop until there is NO overlapping, end < start of new interval
+            while(!q.empty() && q.front() < it.first){
+                q.pop();
+            }
+            
+            // push only end time in queue
+            q.push(it.second);
+            maxSize= max(maxSize, (int) q.size());
+        }
+        return maxSize;
+    }
+};
+
+ 
+
+
+
+
+
+
+
+
+
+// Approach 1.1 ->> Using deque
+// TC= O(N* log N)+ O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -34,3 +85,5 @@ public:
       return maxSize;   
     }
 };
+
+*/
