096845ab4ff9ca5abb75e9a6c60655f7b9266714
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-19 13:10:22 +0530

    Time: 460 ms (7.88%) | Memory: 212.1 MB (5.03%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,13 +1,36 @@
+// Approach 1 -> Find max no of overlapping intervals for each element(nums[i]-k, nums[i]+k)
+// TC= O(N* log N)+ O(2*N)
+
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        int n = nums.size(), res = 0;
-        sort(nums.begin(), nums.end());
-        int l = 0, r = 0;
-        while (r < n) {
-            while (r < n && nums[r]-nums[l] <= 2*k) r++, res = max(res, r-l);
-            l++;
+        int n= nums.size();
+
+        vector<vector<int>> intervals(n);
+        for(int i=0; i<n; i++)
+        {
+            intervals[i].push_back(nums[i]-k);
+            intervals[i].push_back(nums[i]+k);
         }
-        return res;
+
+        // sort intervals based on start time in ascending order...
+        sort(intervals.begin(),intervals.end());
+
+        deque<pair<int,int>> dq;
+        int maxSize= 0;
+
+        for(vector<int> &it: intervals)
+        {
+            // pop_front() until end time of front < start time of new interval, NO Overlapping
+            while(!dq.empty() && dq.front().second < it[0])
+            {
+                dq.pop_front();
+            }
+
+            dq.push_back({it[0],it[1]});
+            
+            maxSize= max(maxSize, (int) dq.size());
+        }
+      return maxSize;   
     }
 };
