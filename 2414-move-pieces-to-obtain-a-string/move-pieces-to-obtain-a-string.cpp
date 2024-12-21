f3a09d7ba2b56f51346eb2f3bd31094edeee6e03
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-21 12:53:42 +0530

    Time: 5 ms (85.59%) | Memory: 21.9 MB (38.56%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -1,24 +1,122 @@
-class Solution {
-public:
-    bool canChange(string s, string t) {
-        int n= s.size();
-        int i=0,j=0;
-        while(i<n || j<n){
-          while(i<n && s[i]=='_') i++;
-          while(j<n && t[j]=='_')j++;
-            if(i==n && j==n) return true;
+// Optimized Approach ->> 2 Pointers
+// TC= O(N)
 
-            if(s[i]!=t[j]) return false;
+class Solution{
+  public:
+    bool canChange(string start, string target) {
+        int n= start.length();
 
-            if(s[i]=='L' && i<j) return false;
-            if(s[i]=='R' && i>j) return false;
+        int i=0, j= 0;
+        while(i< n || j < n)
+        {
+            // skip the spaces _ in start string
+            while(i< n && start[i]== '_'){
+                i++;
+            }
+
+            // skip the spaces in target string
+            while(j< n && target[j]== '_'){
+                j++;
+            }
+            
+            // if both strings are finished, return true
+            if(i== n && j== n){
+                return true;
+            }
+            
+            // if any one of the string got finished but other is NOT, return false
+            if(i== n || j== n){
+              return false;
+            }
+ 
+            if(start[i] != target[j]){
+                return false;
+            }
+            
+            // start= 'L_'   target= '_L', we cannot make start = target
+            if(start[i]== 'L' && i<j)
+            {
+                return false;
+            }
+
+            // start= '_R'  target= 'R_', we cannot make start= target
+            if(start[i]== 'R' && i>j)
+            {
+                return false;
+            }
+
             i++;
             j++;
-
         }
-        return true;
-
-
-
+       return true; 
     }
 };
+
+
+
+
+
+
+
+
+//  ----- TLE ------
+// Brute Force Approach
+// TC= O(2^N)
+
+/*
+
+class Solution {
+public:
+    bool solve(string &start, string &target, unordered_map<string,bool> &mp)
+    {
+        int n= start.length();
+        if(start== target)
+        {
+            return true;
+        }
+        
+        // if this state is already known
+        if(mp.count(start))
+        {
+            return mp[start];
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(i-1 >= 0 && start[i]== 'L' && start[i-1]== '_')
+            {
+                swap(start[i-1],start[i]);
+
+                if(solve(start,target,mp)){
+                    return true;
+                }
+                 
+                // backtracking...
+                swap(start[i-1],start[i]);
+            }
+
+            else if(i+1 < n && start[i]== 'R' && start[i+1]== '_')
+            {
+                swap(start[i],start[i+1]);
+
+                if(solve(start,target,mp)){
+                    return true;
+                }
+
+                // backtracking...
+                swap(start[i+1],start[i]);
+            }
+        }
+    return mp[start]= false;
+    }
+
+
+    bool canChange(string start, string target) {
+        int n= start.length();
+
+        unordered_map<string,bool> mp;
+        return solve(start,target,mp);
+    }
+};
+
+*/
