0927e8e4d22cc901aaca6ccf521fac3bc7c05c9e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-22 00:18:44 +0530

    Time: 144 ms (50.00%) | Memory: 186.3 MB (29.75%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,4 +1,59 @@
+// Approach 2-> DFS using visited vector
+
+class Solution{
+public:
+    int dfs(int currNode, vector<vector<int>> &adjList, vector<bool> &visited,vector<int> &values,int &k, long long &sum)
+    {
+        visited[currNode]= true;
+        sum+= values[currNode];
+        int cnt= 0;
+   
+        for(auto neighbour: adjList[currNode])
+        {
+            if(!visited[neighbour])
+            {
+                long long childSum= 0;
+                cnt+= dfs(neighbour, adjList, visited,values,k, childSum);
+                sum+= childSum;
+            }
+        }
+
+        if(sum % k==0)
+        {
+            cnt++;
+            sum= 0;
+        }
+        return cnt;
+    }
+
+
+
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        // create adjList
+        vector<vector<int>> adjList(n);
+
+        for(auto it: edges)
+        {
+            adjList[it[0]].push_back(it[1]);
+            adjList[it[1]].push_back(it[0]);
+        }
+
+        vector<bool> visited(n,false);
+        long long sum= 0;
+
+        return dfs(0,adjList,visited,values,k,sum);
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> DFS without using visited vector
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -40,3 +95,5 @@ public:
         return dfs(0,-1,adjList,values,k,sum);
     }
 };
+
+*/
