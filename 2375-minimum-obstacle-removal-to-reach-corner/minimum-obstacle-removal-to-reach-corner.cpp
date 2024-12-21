0ce9fa1b466228bc5332fb0c855c3d3879da3dda
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-21 18:14:20 +0530

    Time: 230 ms (48.21%) | Memory: 108.8 MB (73.89%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,31 +1,58 @@
+// Approach 1 ->> Floyd warshall
+// TC= O(E* log V) where E-> No of edges, V-> Vertices
+ 
+
 class Solution {
 public:
-    int minimumObstacles(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<int>>dist(rows ,vector<int>(cols ,1e9));
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        vector<int> directions = {-1, 0, 1, 0, -1};
-        pq.emplace(0, 0, 0);
-        dist[0][0] = 0;
-        while(pq.size()>0)
-        {
-            auto [cost, x, y] = pq.top();
-            pq.pop();
-            if (x == rows - 1 && y == cols - 1) return cost;
-            for (int k = 0; k < 4; ++k) {
-                int nx = x + directions[k];
-                int ny = y + directions[k + 1];
+#define P pair<int, pair<int, int>>
 
-                if (nx >= 0 && ny >= 0 && nx < rows && ny < cols) {
-                    int newCost = cost + grid[nx][ny];
-                    if (newCost < dist[nx][ny]) {
-                        dist[nx][ny] = newCost;
-                        pq.emplace(newCost, nx, ny);
-                    }
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        // min heap to store ->> {wt, {x,y}}
+        priority_queue<P, vector<P>, greater<P>> pq;
+        pq.push({0, {0, 0}});
+
+        vector<vector<int>> minDist(n, vector<int>(m, INT_MAX));
+        minDist[0][0] = 0;
+
+        vector<int> dirX = {-1, 0, 1, 0};
+        vector<int> dirY = {0, 1, 0, -1};
+
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            pq.pop();
+
+            if(x== n-1 && y== m-1)
+            {
+                return dist;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                int newX = x + dirX[k];
+                int newY = y + dirY[k];
+
+                if (newX < 0 || newY < 0 || newX >= n || newY >= m){
+                    continue;
                 }
+
+                int wt = 0;
+                if(grid[newX][newY] == 1){
+                    wt = 1;
+                }
+
+                if (dist + wt < minDist[newX][newY])
+                {
+                    minDist[newX][newY]= dist+ wt;
+                    pq.push({dist+wt, {newX,newY}});
+                }
+
             }
         }
-        return -1;
+
+        return minDist[n-1][m-1];
     }
 };
