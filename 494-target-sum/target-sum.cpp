e0ea1e0753479f4d1a87237d19e631aa056a40d3
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2024-12-26 13:21:19 +0530

    Time: 15 ms (59.97%) | Memory: 26.3 MB (25.06%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,5 +1,54 @@
-// Approach 1 -> Backtracking
+// Approach 2.1 -> Top Down DP (Memoization)
+// TC= O[N* (2* TotalSum + 1)] ->> O(N* totalSum)
+
+class Solution{
+ public: 
+    int solve(int index, vector<int> &nums, int currSum,int &totalSum,int &target, vector<vector<int>> &dp)
+    {
+        if(index== nums.size())
+        {
+            return (currSum== target) ? 1 : 0;
+        }
+        
+        // currSum can be -ve, so we add totalSum in it to store in 2D vector
+        if(dp[index][currSum+ totalSum] != -1) 
+        {
+            return dp[index][currSum+ totalSum];
+        }
+
+        int plus= solve(index+1, nums, currSum+ nums[index], totalSum, target,dp);
+        int minus= solve(index+1, nums, currSum- nums[index], totalSum, target, dp);
+
+        return dp[index][currSum+ totalSum]= plus+ minus;
+    }
+     
+
+     int findTargetSumWays(vector<int>& nums, int target) {
+        int n= nums.size();
+        int totalSum= 0;
+
+        for(int i=0; i<n; i++){
+          totalSum+= nums[i];
+        }
+        
+        // currSum can be -ve, so we cannot store it directly in 2D vector as index cannot be -ve
+        vector<vector<int>> dp(n+1, vector<int> (2*totalSum+1, -1));
+
+        int currSum= 0;
+        return solve(0,nums,currSum,totalSum,target,dp);
+     }
+};
+
+
+
+
+
+
+
+// Approach 1 -> Exploring both combinations for every index
 // TC= O(2^N)
+
+/*
 
 class Solution {
 public:
@@ -29,3 +78,5 @@ public:
         return solve(0,nums,currSum, target);
     }
 };
+
+*/
