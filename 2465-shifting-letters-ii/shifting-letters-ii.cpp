007a4a02e9c47336d6cf81c95681d3377f4f69de
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-01-06 16:14:56 +0530

    Time: 1 ms (86.05%) | Memory: 98.5 MB (50.19%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,31 +1,118 @@
+/*
+   **** [DIFFERENCE ARRAY METHOD] ****
+    
+    Query=  [L, R, val]
+
+    diff[L] += val
+    diff[R+1] -= val
+
+    TC= O(N), Instead of again and again iterating array for diff indices, [To prevent TC= O(N*Q)]
+    directly do ans[L]+= val and ans[R+1] -= val
+    and then find cummulative sum which will give final changes required..  
+*/
+
+
+// Approach 2 -> Optimized
+// Difference Array Method (of MIK)
+// TC= O(N + Q)
+
 class Solution {
 public:
-    string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.length();
-        vector<int> shiftLog(n,0);
-        for(int i = 0; i < shifts.size(); i++){
-            if(shifts[i][2]==0){
-                shiftLog[shifts[i][0]]-=1;
-                if(shifts[i][1]+1 < n){
-                    shiftLog[shifts[i][1]+1]+=1;
-                }
-            }else{
-                shiftLog[shifts[i][0]]+=1;
-                if(shifts[i][1]+1 < n){
-                    shiftLog[shifts[i][1]+1]-=1;
-                }
+    string shiftingLetters(string str, vector<vector<int>>& shifts) {
+        int n= str.length();
+        
+        vector<int> diff(n,0);
+        for(int i= 0; i<shifts.size(); i++)
+        {
+           int leftIdx=  shifts[i][0];
+           int rightIdx= shifts[i][1];
+
+           // if shifts[i][2]== 0, move leftwards...
+           int val= -1; 
+           
+           // moving rightwards..
+           if(shifts[i][2]== 1){
+              val= 1;
+           }
+
+            diff[leftIdx]+= val;
+
+            if(rightIdx+ 1 < n){
+                diff[rightIdx + 1] -= val;
             }
-        } 
-        int prSum = 0;
-        for(auto& it:shiftLog){
-            prSum += it;
-            it = prSum;
         }
-        for(int i = 0; i < n; i++){
-            shiftLog[i]%=26;
-            if(shiftLog[i] < 0) shiftLog[i]+=26;
-            s[i] = 'a' + (s[i]-'a'+shiftLog[i])%26;
+        
+        int currSum= 0;
+        string ans= "";
+
+        for(int i=0; i<n; i++)
+        {
+            currSum= (currSum + diff[i]) % 26;
+            if(currSum < 0){
+                currSum+= 26;
+            }
+
+            int temp= ((str[i]-'a') + currSum) % 26;
+            ans+= (temp+ 'a');
         }
-        return s;
+        return ans; 
     }
 };
+ 
+
+
+
+
+
+
+
+
+
+//  ----- TLE ------
+// Approach 1 ->> Brute Force Aproach 
+// TC= O(N* Q)
+
+/*
+
+class Solution {
+public:
+    string shiftingLetters(string str, vector<vector<int>>& shifts) {
+        int n= str.length();
+
+        vector<int> diff(n,0);
+        for(int i=0; i<shifts.size(); i++)
+        {
+            int leftIdx= shifts[i][0];
+            int rightIdx= shifts[i][1];
+            int val= shifts[i][2];
+
+            for(int j= leftIdx; j<= rightIdx; j++)
+            {
+                // moving leftwards..
+                if(val== 0){
+                  diff[j]-= 1;
+                }
+
+                // moving rightwards...
+                else{
+                  diff[j]+= 1;
+                }
+            }
+        }
+
+        string ans= "";
+        for(int i=0; i<n; i++)
+        {
+            int shift= diff[i] % 26;
+             
+             // OR we can also do..
+             // if(shift < 0) shift+= 26;
+
+            int temp= (((str[i]-'a') + shift)+ 26) % 26;
+            ans+= temp + 'a';
+        }
+       return ans; 
+    }
+};
+
+*/
