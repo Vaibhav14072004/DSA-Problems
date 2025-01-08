5fb242db9fd3e20307a024b2bca7ecb9fc844d41
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-01-08 14:16:43 +0530

    Time: 3 ms (68.22%) | Memory: 23.5 MB (78.18%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,5 +1,47 @@
-// Approach -> Brute Force Approach
+// Approach 1.2 ->> Brute Force Approach => Using str.find() function
+// TC= O(N^3)
+
+class Solution{
+public:
+bool isPrefixSuffix(string strA,string strB)
+{
+    int lenA= strA.length();
+    int lenB= strB.length();
+    if(strB.find(strA)== 0 && strB.rfind(strA)== (lenB-lenA))
+    {
+        return true;
+    }
+   return false; 
+}
+
+ int countPrefixSuffixPairs(vector<string>& words) {
+        int n= words.size();
+        int cnt= 0;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j= i+1; j<n; j++)
+            {
+                if(isPrefixSuffix(words[i],words[j]))
+                {
+                    cnt++;
+                }
+            }
+        }
+      return cnt;  
+    }
+};
+
+
+
+
+
+
+
+// Approach 1.1 -> Brute Force Approach => Using for loop
 // TC= O[N*N*N] ->> O(N^3)
+
+/*
 
 class Solution {
 public:
@@ -8,6 +50,8 @@ public:
     {
          int lenA= a.length();
          int lenB= b.length();
+
+         if(lenA > lenB) return false;
 
          for(int i=0; i<lenA; i++)
          {
@@ -36,3 +80,5 @@ public:
       return cnt;  
     }
 };
+
+*/
