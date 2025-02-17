3c86bb7f6de789115c4938b2347d42a69a9eab09
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-17 15:19:48 +0530

    Time: 80 ms (95.43%) | Memory: 91.4 MB (71.63%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,21 +1,30 @@
+// TC= O(N log N) + O(log N)
+// SC= O(N)
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int n=nums.size();
-        priority_queue<long,vector<long>,greater<long>> pq;
-        for(int x:nums){
-            pq.push(x);
+        int n= nums.size();
+        
+        // min heap 
+        // heapify ->> O(log n)
+        priority_queue<long long,vector<long long>,greater<long long>> pq(nums.begin(),nums.end());
+        int cnt= 0;
+        while(!pq.empty())
+        {
+            if(pq.top() >= k){
+                break;
+            }
+
+           long long element1= pq.top();
+           pq.pop();
+           long long element2= pq.top();
+           pq.pop();
+           
+           long long newElement= 2* element1 + element2;
+           pq.push(newElement);
+           cnt++;
         }
-        int minop=0;
-        while(pq.top()<k&&pq.size()>=2){
-            long long x=pq.top();
-            pq.pop();
-            long long y=pq.top();
-            pq.pop();
-            long long val=2*x+y;
-            pq.push(val);
-            minop++;
-        }
-        return minop;
+    return cnt;
     }
 };
