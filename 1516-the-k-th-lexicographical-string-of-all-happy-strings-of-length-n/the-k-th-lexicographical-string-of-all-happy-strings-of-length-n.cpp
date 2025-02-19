abcf3f05773aec768673d26c32822be5280b9e37
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-19 16:22:29 +0530

    Time: 4 ms (63.82%) | Memory: 7.8 MB (99.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,9 +1,65 @@
 // Ques of Backtracking... (DO, EXPLORE, UNDO)
+
+// Approach 2 ->> Using cnt variable...
+// TC= O[3* 2^(N-1)]
+// SC= O(N) for recursive stack space
+
+class Solution{
+  public:
+    void solve(int &cnt,string &ans,string &curr,int n, int k)
+    {
+        // base case
+        if(curr.size()== n)
+        {
+            cnt++;
+            if(cnt== k)
+            {
+                ans= curr;
+            }
+            return;
+        }
+
+        for(char ch= 'a'; ch<= 'c'; ch++)
+        {
+            // adjacent chars should NOT be equal..
+            if(!curr.empty() && curr.back()== ch){
+                continue;
+            }
+
+            curr+= ch;
+            solve(cnt,ans,curr,n,k);
+            
+            // backtracking...
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k){
+        int cnt= 0;
+        string ans= "";
+        string curr= "";
+        solve(cnt,ans,curr,n,k);
+        return ans;  
+    }
+};
+
+
+
+
+
+
+
+
+
+// Approach 1 -> Storing all strings ..
+
 // TC= O[3* 2*2*2..... upto n-1] ->> 3* 2^(N-1)
-// SC= O[N* 2^N] bcoz there will be 2^N strings in ans vector and each string is of size n
+// SC= O(N) for recursive stack + O[N* 2^N] bcoz there will be 2^N strings in ans vector and each string is of size n
 
 // TC= O[1.5* 2^N] ->> O(2^N)
 // SC= O(N* 2^N)
+
+/*
 
 class Solution {
 public:
@@ -21,10 +77,10 @@ public:
             if(!curr.empty() && curr.back()== ch){
                 continue;
             }
-            curr+= ch;
-            solve(ans,curr,n,k);
+            curr+= ch;           // DO
+            solve(ans,curr,n,k); // EXPLORE
 
-            // backtracking...
+            // backtracking...  UNDO
             curr.pop_back();
         }
     }
@@ -40,3 +96,5 @@ public:
         return ans[k-1];
     }
 };
+
+*/
