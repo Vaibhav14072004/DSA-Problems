b7b99afb962079c2f98ce35d0e4fde6346027019
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-20 17:36:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (70.64%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,7 +1,46 @@
+// Approach 2 -> Using Swap function, Inplace algorithm without using separate curr string
+// TC= O(N* N!)
+// SC= O(N* N!) + O(N) for recursive stack
+
+class Solution {
+public:
+    void solve(int idx,vector<int> &nums,vector<vector<int>> &ans)
+    {
+        // base case
+        if(idx >= nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        
+        // if we start loop from i=0, we will got same already used vectors again and again
+        for(int i= idx; i<nums.size(); i++)
+        {
+            swap(nums[i],nums[idx]);
+            solve(idx+1,nums,ans);
+            
+            // backtrack..
+            swap(nums[i],nums[idx]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n= nums.size();
+        vector<vector<int>> ans;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
+
+
+
+
+
 // Approach 1 -> Generating and Storing all permutations
 // TC= O(N* N!)
-// SC= O(N* N!) + O(N)
+// SC= O(N* N!) + O(N) for recursive stack space
 
+/*
 class Solution {
 public:
     void solve(vector<int> &nums,vector<int> &curr,vector<vector<int>> &ans, vector<bool> &used)
@@ -13,7 +52,7 @@ public:
             return;
         }
         
-        // here order does not matters, [1,2,3] m 1 can come even after 2
+        // here order does not matters, In [1,2,3], 1 can come even after 2
         for(int j=0; j<nums.size(); j++)
         {
             if(used[j]) {
@@ -40,3 +79,5 @@ public:
         return ans;
     }
 };
+
+*/
