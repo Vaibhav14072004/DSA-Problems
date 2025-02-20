f2b04418df4bcbde8174276f955d6c45ba42edd8
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-20 11:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (38.12%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,5 +1,54 @@
-// Approach 1 ->> Just put the opposite of every digit in ans
+// Approach 2 -> BackTracking...
+// TC= O(2^N)
+// SC= O(N^2) + O(N) for recursive stack space
+
+class Solution{
+public:
+    void solve(vector<string> &nums, string &curr, string &ans,unordered_set<string> &st)
+    {
+        // base case
+        if(!ans.empty()) return;
+
+        if(curr.size()== nums.size())
+        {
+            if(!st.count(curr))
+            {
+                ans= curr;
+            }
+           return; 
+        }
+        
+        // First try to append '0' and call recursion
+        curr+= '0';
+        solve(nums,curr,ans,st);
+        curr.pop_back();
+        
+        // Then try to append '1' and call recursion
+        curr+= '1';
+        solve(nums,curr,ans,st);
+        curr.pop_back();
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n= nums.size();
+        string ans= "";
+        string curr= "";
+        unordered_set<string> st(nums.begin(),nums.end());
+        solve(nums,curr,ans,st);
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+// Optimized Best Approach 1 ->> Just put the opposite of every digit in ans
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -21,3 +70,5 @@ public:
     return ans;
     }
 };
+
+*/
