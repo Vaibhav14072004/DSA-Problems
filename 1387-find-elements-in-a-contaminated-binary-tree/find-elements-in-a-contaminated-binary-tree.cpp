8b4fecc3a50311b551317de0034aa371c2c40e69
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-21 15:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (75.89%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -10,7 +10,67 @@
  * };
  */
 
+// Approach 2 ->> BFS
+// TC= O(N)
+
+class FindElements {
+public:
+    unordered_set<int> st;
+
+    void BFS(TreeNode* root,int x)
+    {
+        if(root== NULL){
+            return;
+        }
+
+        queue<TreeNode*> q;
+        root->val= x;
+        q.push(root);
+        st.insert(x);
+
+        while(!q.empty())
+        {
+            TreeNode* node= q.front();
+            int value= node->val;
+            q.pop();
+            
+            if(node->left)
+            {
+                node->left->val= (2*value)+1;
+                q.push(node->left);
+                st.insert(node->left->val);
+            }
+
+            if(node->right)
+            {
+                node->right->val= (2*value)+ 2;
+                q.push(node->right);
+                st.insert(node->right->val);
+            }
+        }
+    }
+
+    FindElements(TreeNode* root) {
+       st.clear();
+       BFS(root,0);
+    }
+    
+    bool find(int target) {
+       return st.count(target); 
+    }
+};
+
+
+
+
+
+
+
+
  // Approach 1 -> DFS
+ // TC= O(N)
+
+/*
 
 class FindElements {
 public:
@@ -36,6 +96,8 @@ public:
        return st.count(target); 
     }
 };
+
+*/
 
 
 /**
