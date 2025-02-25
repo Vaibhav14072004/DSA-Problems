4df7be7c4a3de737403ee8bd96e9e0e1e80229d3
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-25 07:43:32 +0530

    Time: 939 ms (19.21%) | Memory: 98.2 MB (5.15%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,22 +1,31 @@
+// Brute Force Approach ->> TC= O(K) for getProduct func
+
 class ProductOfNumbers {
 public:
-    vector<int> stream;
+    vector<int> arr;
+
     ProductOfNumbers() {
-        stream.push_back(1);  // Initialize with 1 to handle division easily.
     }
-
+    
     void add(int num) {
-        if (num == 0) {
-            stream = {1};  // Reset if zero is encountered.
-        } else {
-            stream.push_back(stream.back() * num);
-        }
+      arr.push_back(num);
     }
-
+    
     int getProduct(int k) {
-        if (k >= stream.size()) {
-            return 0;  // If k is out of bounds, return 0 (zero encountered previously).
-        }
-        return stream.back() / stream[stream.size() - 1 - k];
+         int prod= 1;
+         int n= arr.size();
+         for(int i=n-k; i<n; i++)
+         {
+            prod= prod* (arr[i]);
+         }
+       return prod;   
     }
 };
+
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
