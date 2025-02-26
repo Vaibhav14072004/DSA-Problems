0945091a84c27a2031e5acf0df7c8ce932f7c8cc
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-26 21:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.38%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,3 +1,88 @@
+// Approach 2.2 ->> Bottom Up
+// TC= O(N*M)
+
+
+class Solution{
+public:
+   int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m,vector<int> (n,0));
+       dp[m-1][n-1]= 1;
+
+       for(int i=m-1; i>=0; i--)
+       {
+          for(int j= n-1; j>=0; j--)
+          {
+              if(i== m-1 && j== n-1) continue;
+              
+              // moving down..
+              if(i+1 < m) 
+              {
+                 dp[i][j]+= dp[i+1][j];
+              }
+
+              // moving right
+              if(j+1 < n)
+              {
+                 dp[i][j]+= dp[i][j+1];
+              }
+          }
+       }
+     return dp[0][0];  
+   }
+};
+
+
+
+
+
+
+
+
+// Approach 2.1 -> Bottom Up  
+// TC= O(N*M)
+
+/*
+
+class Solution{
+public:
+   int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m,vector<int> (n,0));
+
+       dp[0][0]= 1;
+       for(int i=0; i<m; i++)
+       {
+           for(int j=0; j<n; j++)
+           {
+               if(i==0 && j== 0) continue;
+
+               // moving up...
+               int up= 0,left= 0;
+               
+               if(i-1 >= 0) up= dp[i-1][j];
+               if(j-1 >= 0) left= dp[i][j-1];
+
+               dp[i][j]= up+ left;
+           }
+       }
+     return dp[m-1][n-1];  
+   }
+};
+
+*/
+
+
+
+
+
+
+
+
+// Approach 1 -> Top Down
+// TC= O(M*N)
+// SC= O(M*N) + O(M+N)
+
+/*
+
 class Solution {
 public:
     int solve(int i, int j,int m, int n, vector<vector<int>> &dp)
@@ -18,3 +103,5 @@ public:
         return solve(0,0,m,n,dp);
     }
 };
+
+*/
