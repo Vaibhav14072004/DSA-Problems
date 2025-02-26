1205eb1996837ae4bb03ce7e806ac006823f0198
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-02-26 14:43:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (20.65%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,7 +1,45 @@
-// Approach 2 ->> Kadane Algorithm
-// Find max out of [maxSubarraySum, abs(minSubarraySum)]
+// Approach 3 -> One pass Kadane Algorithm
+// Find max out of [maxSubarraySum, abs(minSubarraySum)] in one paas only
 // TC= O(N)
 
+class Solution{
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n= nums.size();
+        int maxSum= INT_MIN;
+        int minSum= INT_MAX;
+        int currSum_max= 0;
+        int currSum_min= 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(currSum_max < 0)
+            {
+                currSum_max= 0;
+            }
+            currSum_max+= nums[i];
+            maxSum= max(maxSum, currSum_max);
+
+            if(currSum_min > 0)
+            {
+                currSum_min= 0;
+            }
+            currSum_min+= nums[i];
+            minSum= min(minSum, currSum_min);
+        }
+    return max(maxSum, abs(minSum));
+    }
+};
+
+
+
+
+
+// Approach 2 ->> Kadane Algorithm
+// Find max out of [maxSubarraySum, abs(minSubarraySum)]
+// TC= O(2*N)
+
+/*
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
@@ -35,6 +73,10 @@ public:
     return max(maxSum, abs(minSum));
     }
 };
+
+*/
+
+
 
 
 
