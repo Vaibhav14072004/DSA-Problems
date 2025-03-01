ea6ed6118057a0c7647179f45b7f9eb24607f72e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-01 14:49:51 +0530

    Time: 34 ms (6.91%) | Memory: 27 MB (22.09%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,39 +1,79 @@
+// Using Bottom up approach -> (Length of shortest common supersequence)
+// TC= O(N* M) for filling dp table + O(N + M) for making str from dp table
+
+// TC= O(N*M) + O(N+M)
+// SC= O(N*M)
+
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size(), m = str2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+        int n= str1.length();
+        int m= str2.length();
+        vector<vector<int>> dp(n+1,vector<int> (m+1));
+        
+        for(int len1=0; len1<= n; len1++)
+        {
+            for(int len2=0; len2<= m; len2++)
+            {
+                // to handle base case -> if one string is finished,add other remaining string length
+                if(len1== 0 || len2== 0)
+                {
+                    dp[len1][len2]= len1+ len2;
                 }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+
+                else if(str1[len1-1]== str2[len2-1])
+                {
+                    dp[len1][len2]= 1 + dp[len1-1][len2-1];
+                }
+                else
+                {
+                    dp[len1][len2]= 1+ min(dp[len1-1][len2], dp[len1][len2-1]);
                 }
             }
         }
-        int i = n,j = m;
-        string ans = "";
-        while(i>0 && j>0){
-            if(str1[i-1] == str2[j-1]){
-                ans += str1[i-1];
-                i--,j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                ans += str1[--i];
-            }
-            else{
-                ans += str2[--j];
-            }
+        
+        string ans= "";
+        int i=n, j= m;
+
+        while(i > 0 && j> 0)
+        {
+           if(str1[i-1]== str2[j-1])
+           {
+               ans+= str1[i-1];
+               i--;
+               j--;
+           }
+           else
+           {
+               if(dp[i-1][j] < dp[i][j-1])
+               {
+                   ans+= str1[i-1];
+                   i--;
+               }
+               else
+               {
+                   ans+= str2[j-1];
+                   j--; 
+               }
+           }
         }
-        while(i > 0){
-            ans += str1[--i];
+
+        // if 1 string is finished but other is NOT
+        while(i > 0)
+        {
+            ans+= str1[i-1];
+            i--;
         }
-        while(j > 0){
-            ans += str2[--j];
+
+        while(j> 0)
+        {
+            ans+= str2[j-1];
+            j--;
         }
+
+        // reverse the final string...
         reverse(ans.begin(),ans.end());
-        return ans;
+
+      return ans;  
     }
 };
