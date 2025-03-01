0e39d5700190a6c5bcae66c3ee914cea3390e317
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-01 18:19:45 +0530

    Time: 11 ms (97.48%) | Memory: 9 MB (96.22%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,6 +1,85 @@
+// Approach 3 -> Space Optimized
+// TC= O(N*M)
+// SC= O(1)
+
+class Solution{
+ public:
+     int longestCommonSubsequence(string text1, string text2) {
+        int n= text1.size();
+        int m= text2.size();
+
+        int len= max(n,m);
+
+        vector<int> curr(len+1,0);
+        vector<int> next(len+1,0);
+
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j= m-1; j>=0; j--)
+            {
+                if(text1[i]== text2[j])
+                {
+                    curr[j]= 1 + next[j+1];
+                }
+                else
+                {
+                    curr[j]= max(next[j], curr[j+1]);
+                }
+            }
+           next= curr; 
+        }
+     return next[0];
+     }
+};
+
+
+
+
+
+
+// Approach 2 -> Bottom Up (Tabulation)
+// TC= O(N*M)
+// SC= O(N*M) 
+
+/*
+
+class Solution{
+ public:
+     int longestCommonSubsequence(string text1, string text2) {
+        int n= text1.size();
+        int m= text2.size();
+
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j= m-1; j>=0; j--)
+            {
+                if(text1[i]== text2[j])
+                {
+                    dp[i][j]= 1 + dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j]= max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+     return dp[0][0];
+     }
+};
+
+*/
+
+
+
+
+
+
 // Approach 1 -> Top Down (Memoization)
 // TC= O(N*M)
 // SC= O(N*M) + O(N+M) for recursive stack space
+
+/*
 
 class Solution {
 public:
@@ -36,3 +115,5 @@ public:
         return solve(0,0,text1,text2,dp,n,m);
     }
 };
+
+*/
