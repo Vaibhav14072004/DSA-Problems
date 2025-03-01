d276c8566b67db1d6d16a544614687d8cff68b0c
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-01 12:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (87.59%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,6 +1,52 @@
-// Approach 1 ->
-// TC= O(3*N)
+// Approach 2 -> Without using extra space (Implace substitution)
+// TC= O(2*N)
+// SC= O(1)
 
+class Solution{
+ public: 
+   vector<int> applyOperations(vector<int>& nums) {
+        int n= nums.size();
+
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i]== nums[i+1])
+            {
+                nums[i]= 2* nums[i];
+                nums[i+1]= 0;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        // swapping all zeroes wih non zero elements to take all zeroes to end
+        int i=0;  // for zero element
+        int j= 0; // for non zero element
+        while(j < n)
+        {
+            if(nums[j] != 0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+            }
+          j++;
+        }
+    return nums;
+   }
+};
+
+
+
+
+
+
+
+// Approach 1 -> Using extra space 
+// TC= O(3*N)
+// TC= O(N)
+
+/*
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
@@ -41,3 +87,5 @@ public:
     return ans;
     }
 };
+
+*/
