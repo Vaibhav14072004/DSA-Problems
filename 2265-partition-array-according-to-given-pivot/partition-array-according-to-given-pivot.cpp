48b85911c108ec80de83d77bdb44100f54501dbc
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-03 15:27:35 +0530

    Time: 8 ms (50.55%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,6 +1,49 @@
+// Approach 2 -> Without using extra space
+// TC= O(2*N)
+// SC= O(1)
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n= nums.size();
+        vector<int> ans(n,pivot);
+        
+        // first only push elements in ans vector that are less than pivot
+        int idx= 0;
+        for(int i=0; i<n; i++)
+        {
+           if(nums[i] < pivot)
+           {
+               ans[idx]= nums[i];
+               idx++;
+           }
+        }
+
+        // now push elements in ans vector from end that are greater than pivot
+        idx= n-1;
+        for(int i=n-1; i>=0; i--)
+        {
+            if(nums[i] > pivot)
+            {
+                ans[idx]= nums[i];
+                idx--;
+            }
+        }
+      return ans;  
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 ->> Using 2 extra vectors
 // TC= O(2*N)
 // SC= O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -44,3 +87,5 @@ public:
       return ans;  
     }
 };
+
+*/
