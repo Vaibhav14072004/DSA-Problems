f04ac618bc0e00cf6e80da57f553a54e91e437ae
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-07 19:55:43 +0530

    Time: 77 ms (43.48%) | Memory: 75.6 MB (30.26%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,7 +1,82 @@
+// Approach 2 ->> Modified Topological Sort using BFS + Map
+// (Queue , Indegree vector for topo sort and unordered_map for maintaing the transitive property)
+
+// TC= O[V* (V+E)] bcoz we are iterating on map again to fill transitive dependency so we multiply V
+
+class Solution{
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) 
+    {
+         vector<int> indegree(numCourses,0);
+         
+         // first create adj list
+         vector<vector<int>> adj(numCourses);
+         for(auto it: prerequisites)
+         {
+             adj[it[0]].push_back(it[1]);
+             indegree[it[1]]++;
+         }
+         
+         queue<int> q;
+         for(int i=0; i<numCourses; i++){
+            if(indegree[i]== 0)
+               q.push(i);
+         }
+         
+         // map to store all the previous transitive prerequisites of curr node
+         unordered_map<int,unordered_set<int>> mp;
+
+         while(!q.empty())
+         {
+            int node= q.front();
+            q.pop();
+            
+            for(auto it: adj[node])
+            {
+                // store all previous prequisites of prev node also (transitive dependency) 
+                mp[it].insert(node);
+
+                // Extra TC= O(V) will be multiplied bcoz of
+                for(auto j: mp[node])
+                { 
+                    mp[it].insert(j);  
+                }
+
+                indegree[it]--;
+                if(indegree[it]== 0)
+                {
+                    q.push(it);
+                }
+            }
+         }
+            
+         vector<bool> ans(queries.size(),false);
+         for(int i=0; i<queries.size(); i++)
+         {
+             int src= queries[i][0];
+             int dest= queries[i][1];
+
+             // we have to check whether prerequisite of dest is src or NOT (src -> dest)
+             if(mp[dest].count(src))
+             {
+                ans[i]= true;
+             }
+         }
+    return ans;
+    }
+};
+
+
+
+
+
+
 // Approach 1.2 -> Create Adj list and using BFS 
 // find whether path form src to dest exists or NOT
 
 // TC= O[q* (V+E)]
+
+/*
 
 class Solution{
 public:
@@ -48,6 +123,9 @@ public:
     return ans;
     }
 };
+
+*/
+
 
 
 
