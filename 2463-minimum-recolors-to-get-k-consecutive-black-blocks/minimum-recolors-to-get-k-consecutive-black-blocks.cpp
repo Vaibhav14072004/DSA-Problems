440f11d0c362594a8762e7956b6a26519cdcae21
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-08 10:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (31.01%) - LeetSync

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,62 @@
+// Approach 2 -> Sliding Window Approach
+// TC= O(2*N)
+
+/*
+
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int n= blocks.length();
+
+        int i=0, j=0, cnt= 0, ans= INT_MAX;
+        while(j < n)
+        {
+            if(blocks[j]== 'W'){
+                cnt++;
+            }
+            // shrink the curr window...
+            if(j-i+1 == k)
+            {
+                ans= min(ans, cnt);
+                if(blocks[i]== 'W')
+                {
+                    cnt--;
+                }
+                i++;
+            }
+
+          j++;  
+        }
+      return ans;  
+    }
+};
+
+*/
+
+
+
+
+// Brute Force Approach 
+// TC= O(N*K)
+
+class Solution{
+ public:
+    int minimumRecolors(string blocks, int k) {
+        int n= blocks.length();
+        int ans= INT_MAX;
+
+        for(int i=0; i<= n-k; i++)
+        {
+            int cnt= 0;
+            for(int j=i; j-i+1 <= k; j++)
+            {
+                if(blocks[j]== 'W')
+                {
+                    cnt++;
+                }
+            }
+            ans= min(ans,cnt);   
+        }
+    return ans;  
+    }
+};
