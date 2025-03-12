410690e1b075de2ad32da118c22587d14d35ded5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-12 17:49:24 +0530

    Time: 32 ms (16.71%) | Memory: 103.4 MB (37.58%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,36 +1,49 @@
+// Approach 1 -> Using DFS
+// TC= O(N*M)
+
 class Solution {
 public:
-    int n, m;
-    int solve(vector<vector<int>>& grid, int i, int j) {
-        if (i<0 || i>=n || j<0 || j>=m || grid[i][j]==0){
+    int DFS(int i, int j, vector<vector<int>> &grid, int n, int m)
+    {
+        if(i < 0 || i>= n || j< 0 || j>= m || grid[i][j]== 0){
             return 0;
         }
 
-        int sum = grid[i][j];
-        grid[i][j] = 0;
+        int fish= grid[i][j];
+        grid[i][j]= 0;
 
-        sum +=solve(grid, i+1, j);
-        sum +=solve(grid, i-1, j);
-        sum +=solve(grid, i, j+1);
-        sum +=solve(grid, i, j-1);    
-
-        return sum;
+        vector<int> dirX= {-1,0,1,0};
+        vector<int> dirY= {0,1,0,-1};
+        
+        for(int k=0; k<4; k++)
+        {
+            int newX= i+ dirX[k];
+            int newY= j+ dirY[k];
+            
+            // check id newX, newY are valid..
+            if(newX >= 0 && newX < n && newY >= 0 && newY < m)
+            {
+                fish+= DFS(newX,newY,grid,n,m);
+            }
+        }
+    return fish;
     }
 
     int findMaxFish(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        int maxi = 0;
+        int n= grid.size();
+        int m= grid[0].size();
+        int maxi= 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] > 0) {
-                    int res = solve(grid, i, j);
-                    maxi = max(maxi, res);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] > 0)
+                {
+                    maxi= max(maxi, DFS(i,j,grid,n,m));
                 }
             }
         }
-
-        return maxi;
+       return maxi; 
     }
 };
