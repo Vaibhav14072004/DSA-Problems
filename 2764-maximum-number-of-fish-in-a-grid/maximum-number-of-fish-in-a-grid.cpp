9eeca420d86f528c9cf17151226cf84ce82412f4
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-12 20:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 96.3 MB (69.52%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,5 +1,127 @@
+// Approach 3 ->> Using DSU (Disjoint Set Union)
+// TC of DSU is O(No of cells) ->> O(N*M)
+
+// Overall TC= O[(N*M)* alpha(N*M)]
+
+// To convert 2D array into 1D array -> (i* cols)+ j
+
+class DSU {
+public:
+   vector<int> parent;
+   vector<int> count;
+
+   DSU(int n)
+   {
+      parent.resize(n);
+      count.resize(n,0);
+      for(int i=0; i<n; i++){
+         parent[i]= i;
+      }
+   }
+
+   int findParent(int node)
+   {
+      if(node== parent[node]){
+         return node;
+       }
+      return parent[node]= findParent(parent[node]);
+   }
+
+   void Union(int u,int v)
+   { 
+      int parent_u= findParent(u);
+      int parent_v= findParent(v);
+      if(parent_u== parent_v){
+         return;
+      } 
+
+      if(count[parent_u] > count[parent_v])
+      {
+          parent[parent_v]= parent_u;
+          count[parent_u]+= count[parent_v];
+      }
+      else
+      {
+         parent[parent_u]= parent_v;
+         count[parent_v]+= count[parent_u];
+      }
+   }
+};
+
+
+class Solution{
+ public:
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n= grid.size();    // rows
+        int m= grid[0].size(); // columns
+    
+        vector<int> dirX= {-1,0,1,0};
+        vector<int> dirY= {0,1,0,-1};
+
+        DSU dsu(n*m);
+
+        // fill count array of DSU with value of grid[i][j]
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] > 0)
+                {
+                    // convert 2D array into 1D ->> (i* cols)+j
+                    int idx= (i*m)+j;
+                    dsu.count[idx]= grid[i][j];
+                }
+            }
+        }
+        
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j] > 0)
+                {
+                    // convert 2D index into 1D index->> (i* cols)+j
+                    int idx= (i*m)+j;
+                    
+                    // now check all 4 directions..
+                    for(int k=0; k<4; k++)
+                    {
+                       int newX= i+dirX[k];
+                       int newY= j+dirY[k];
+
+                       if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] > 0)
+                       {
+                       // convert 2D index into 1D index ->> (i*cols)+j
+                       int newIdx= (newX* m)+ newY;
+
+                       int oldParent= dsu.findParent(idx);
+                       int newParent= dsu.findParent(newIdx);
+
+                       if(oldParent != newParent)
+                       {
+                          dsu.Union(oldParent,newParent);
+                       } 
+                     }
+                  }
+                } 
+            }
+        }
+    
+    // maximum count will be answer
+    int ans= *max_element(dsu.count.begin(), dsu.count.end());
+    return ans;
+    }
+};
+
+
+
+
+
 // Approach 2 -> BFS
 // TC= O(N*M)
+
+/*
 
 class Solution {
 public:
@@ -45,6 +167,9 @@ public:
         return maxi;
     }
 };
+
+*/
+
 
 
 
