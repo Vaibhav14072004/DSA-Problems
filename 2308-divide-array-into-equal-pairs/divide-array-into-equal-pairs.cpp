43b6403d571143783147ece4a236ef9d89b19364
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-17 11:53:46 +0530

    Time: 4 ms (42.39%) | Memory: 18.2 MB (31.79%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,5 +1,39 @@
+// Approach 2 -> Using unordered_set
+// TC= O(N)
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n= nums.size();
+        unordered_set<int> st;
+        int cnt= 0;
+
+        for(int i=0; i<n; i++)
+        {
+            // if element is already present in set
+            if(st.find(nums[i]) != st.end())
+            {
+                st.erase(nums[i]);
+                cnt++;
+            }
+            else
+            {
+                st.insert(nums[i]);
+            }
+        }
+       return cnt== n/2; 
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Using unordered_map
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -22,3 +56,5 @@ public:
     return cnt== n/2;
     }
 };
+
+*/
