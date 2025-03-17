538208303324f9305165fb94e4ce9868edf01fd9
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-17 12:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (73.28%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,5 +1,34 @@
+// Approach 3 -> Using XOR
+// XOR of same elements would be 0 but cannot be Ex- 2,3,4,5 XOR is 0 but they are not in pair
+// Therfore first sort the vector, then pairwise take XOR
+
+// TC= O(N* log N) for sorting
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+
+        for(int i=1; i<n; i+= 2)
+        {
+            if((nums[i-1] ^ nums[i]) != 0)
+            {
+                 return false;
+            }
+        }
+    return true;
+    }
+};
+
+
+
+
+
 // Approach 2 -> Using unordered_set
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -24,6 +53,8 @@ public:
        return cnt== n/2; 
     }
 };
+
+*/
 
 
 
