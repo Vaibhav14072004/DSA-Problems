db1490b8c9d1a2faadb141645d60c01bd00e90e4
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-03-19 14:06:02 +0530

    Time: 15 ms (16.43%) | Memory: 140 MB (74.50%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,5 +1,44 @@
+// Approach 2 
+// TC= O(3*N)
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n= nums.size();
+        int ans= 0;
+        if(n < 3) return 0;
+        
+        for(int i=0; i<= n-3; i++)
+        {
+            if(nums[i]== 0)
+            {
+                ans++;
+               for(int j=i; j<= i+2; j++){
+                  nums[j]^= 1;
+               }
+            }
+        }
+
+        // check last 3 elements if they are zero return -1
+        if(nums[n-1]== 0 || nums[n-2]== 0 || nums[n-3]== 0)
+        {
+            return -1;
+        }
+
+      return ans;  
+    }
+};
+
+
+
+
+
+
+
 // Approach 1 
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -32,3 +71,5 @@ public:
       return ans;  
     }
 };
+
+*/
