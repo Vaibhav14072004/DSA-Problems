5e86847c85c7b022dce9d365c1705f2772570c89
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-02 16:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (66.87%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,6 +1,64 @@
+/*
+    i ..... j..... k  ->> (nums[i]-nums[j])* nums[k]
+    so we need to maximize nums[i] as well as nums[k]
+*/
+
+
+// Approach 3 -> Optimized Without using extra space (only one iteration)
+// TC= O(N)
+// SC= O(1)
+
+class Solution{
+ public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n= nums.size();
+        if(n < 3) return 0;
+
+        long long result= 0;
+        int maxDiff= 0, leftMax= 0; 
+        for(int i=0; i<n; i++)
+        {
+            result= max(result, (long long) maxDiff* nums[i]);
+            maxDiff= max(maxDiff, leftMax-nums[i]);
+            leftMax= max(leftMax,nums[i]);
+        }
+      return result;  
+    }
+};
+
+
+
+
+
+/*  -------  WRONG ANS  ----------
+class Solution{
+public:
+   long long maximumTripletValue(vector<int>& nums) {
+        int n= nums.size();
+        long long maxi= 0;
+
+        if(n < 3) return 0;
+        
+        int leftMax= nums[0], rightMax= nums[n-1];
+        for(int i=1; i<n-1; i++){
+            maxi= max(maxi, (long long) (leftMax-nums[i])* rightMax);
+            leftMax= max(leftMax, nums[i]);
+            rightMax= max(rightMax, nums[i+1]);
+        }
+    return maxi;    
+   }};
+*/
+
+
+
+
+
+
 // Approach 2 -> Using leftMax array and rightMax array 
 // Tc= O(3*N)
 // SC= O(2*N)
+
+/*
 
 class Solution{
 public:
@@ -30,6 +88,10 @@ public:
       return maxi;  
     }
 };
+
+*/
+
+
 
 
 
