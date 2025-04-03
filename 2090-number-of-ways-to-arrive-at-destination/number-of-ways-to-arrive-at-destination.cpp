7e21badc4217bcfcd92de3e4e227074f86e48ac1
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-03 18:35:52 +0530

    Time: 10 ms (39.58%) | Memory: 35 MB (60.16%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,38 +1,61 @@
+// Approach ->> Dijsktra Algorithm
+// TC= O(E* log V)
+
 class Solution {
 public:
+    const int MOD= 1000000007;
+
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<long long, long long>> adj[n];
-        for (auto it : roads) {
-            adj[it[0]].push_back({it[1], it[2]});
-            adj[it[1]].push_back({it[0], it[2]});
+
+        // adj list ->> {u, {v,time}}
+        unordered_map<int,vector<pair<int,int>>> adj;
+
+        for(int i=0; i<roads.size(); i++)
+        {
+            int u= roads[i][0];
+            int v= roads[i][1];
+            int dist= roads[i][2];
+
+            adj[u].push_back({v,dist});
+            adj[v].push_back({u,dist});
         }
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
-                       greater<pair<long long, int>>>
-            pq;
-        pq.push({0, 0});
-        vector<long long> dist(n, LONG_MAX);
-        dist[0] = 0;
-        vector<long long> ways(n, 0);
-        ways[0] = 1;
-        long long mod = 1e9 + 7;
-        while (!pq.empty()) {
-            auto it = pq.top();
-            long long node = it.second;
-            long long dis = it.first;
+        
+        // min heap ->> {shortestTime, node}  ... shortestTime must be long long
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
+        pq.push({0,0});
+
+        vector<long long> minTime(n,LLONG_MAX);
+        minTime[0]= 0;
+        
+        // total no of ways to reach that node
+        vector<int> ways(n,0);
+        ways[0]= 1;
+
+        while(!pq.empty())
+        {
+            long long currTime= pq.top().first;
+            int currNode= pq.top().second;
             pq.pop();
 
-            for (auto it : adj[node]) {
-                long long edjnode = it.first;
-                long long edjwt = it.second;
-                if (edjwt + dis < dist[edjnode]) {
-                    dist[edjnode] = edjwt + dis;
-                    pq.push({dist[edjnode], edjnode});
-                    ways[edjnode] = ways[node];
-                } else if (dis + edjwt == dist[edjnode]) {
-                    ways[edjnode] = (ways[edjnode] + ways[node]) % mod;
+            for(auto it: adj[currNode])
+            {
+                int neighbour= it.first;
+                int time= it.second;
+
+                if(currTime+ time < minTime[neighbour])
+                {
+                    minTime[neighbour]= currTime+ time;
+                    pq.push({currTime+time, neighbour});
+                    ways[neighbour]= ways[currNode];
+                }
+
+                // there is one more way to reach that node..
+                else if(currTime+ time == minTime[neighbour])
+                {
+                    ways[neighbour]= (ways[neighbour]+ ways[currNode]) % MOD;
                 }
             }
         }
-        return ways[n - 1] % mod;
+    return ways[n-1];
     }
 };
