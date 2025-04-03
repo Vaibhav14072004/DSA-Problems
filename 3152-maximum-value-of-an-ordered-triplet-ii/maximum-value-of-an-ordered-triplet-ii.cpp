f305b54a6c4c617259fd67ae9682ff9b77b6e9f3
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-03 12:40:01 +0530

    Time: 6 ms (47.63%) | Memory: 90.9 MB (97.21%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -1,6 +1,33 @@
+// Approach 3 -> Optimized Approach (Without using extra space)
+// TC= O(N)
+// SC= O(1)
+
+class Solution{
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n= nums.size();
+        long long result= 0;
+
+        int maxDiff= 0, leftMax= 0;
+        for(int i=0; i<n; i++)
+        {
+            result= max(result, (long long) (maxDiff)* nums[i]);
+            maxDiff= max(maxDiff, leftMax-nums[i]);
+            leftMax= max(leftMax, nums[i]);
+        }
+    return result;
+    }
+};
+
+
+
+
+
 // Approach 2 -> Using 2 extra vectors..
 // TC= O(3*N)
 // SC= O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -30,12 +57,15 @@ public:
     }
 };
 
+*/
+
 
 
 
 
 // Approach 1 -> Brute Force Approach
 // TC= O(N^3)
+// SC= O(1)
 
 /*
 
