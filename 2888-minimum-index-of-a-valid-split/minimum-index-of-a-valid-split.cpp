8f2cd8a7f8052d2d83982ce54f19eec8cac8c65a
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-07 19:39:55 +0530

    Time: 158 ms (5.05%) | Memory: 103.1 MB (10.26%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,37 +1,33 @@
+// Approach 1 -> Using unordered_map
+// TC= O(2*N)
+// SC= O(2*N)
+
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        int ans =-1;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        int dominant;
-        int maxi=0;
-        for(auto i: mp){
-            if(i.second>maxi){
-            maxi = i.second;
-            dominant = i.first;
-            }
+        int n= nums.size(); 
+        unordered_map<int,int> mp1;
+        unordered_map<int,int> mp2;
+
+        for(int i=0; i<n; i++)
+        {
+            mp2[nums[i]]++;
         }
 
-        cout<<dominant<<endl;
-        int countleft=0;
+        for(int i=0; i<n; i++)
+        {
+            mp1[nums[i]]++;
+            mp2[nums[i]]--;
 
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==dominant){
-                countleft++;
+            int n1= i+1;
+            int n2= n-i-1;
+
+            // now check if this index can be a valid split...
+            if(mp1[nums[i]] > n1/2 && mp2[nums[i]] > n2/2)
+            {
+                return i;
             }
-            int countright = maxi-countleft;
-            int lenleft = i+1;
-            int lenright = n-lenleft;
-            if(countleft*2>lenleft && countright*2>lenright){
-                ans=i;
-                return ans;
-            }
-            
         }
-        return ans;
+      return -1;  
     }
 };
