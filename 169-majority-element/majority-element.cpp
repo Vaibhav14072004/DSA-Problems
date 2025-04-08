0d5ac895779df881cd8898efa70edfcb6c0220bd
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-08 18:04:48 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (84.09%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,6 +1,59 @@
+// Approach 2 -> Majority Element (Moore Voting Algorithm)
+// TC= O(2*N)
+// SC= O(1)
+
+class Solution{
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+        int cnt= 0;
+        int majorityElement;
+
+        for(int i=0; i<n; i++)
+        {
+            if(cnt== 0)
+            {
+                cnt= 1;
+                majorityElement= nums[i];
+            }
+            else if(majorityElement== nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+
+        // now cross verify majority element
+        int freq= 0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]== majorityElement)
+            {
+                freq++;
+            }
+        }
+
+     if(freq> n/2)
+     {
+        return majorityElement;
+     }
+
+     return -1;
+    }
+};
+
+
+
+
+
 // Approach 1 -> Using unordered_map
 // TC= O(2*N)
 // SC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -23,3 +76,5 @@ public:
     return -1;
     }
 };
+
+*/
