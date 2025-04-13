941a3dafe2a1c2d9812814d3a196d9c90fbfff41
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-13 18:11:57 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (72.27%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,5 +1,57 @@
+// Approach 2 -> Using maths
+/*
+    long long val1= sum-Sn;     // x-y
+    long long val2= sumSq-Sn2;  // x^2 - y^2
+
+    // val1= x-y
+    // val2= (x+y)*(x-y) => val1* (x+y)
+    // x+y= val2/val1
+    // x-y= val1
+
+    // x= (val2/va1  + val1)/2
+    // y= x - val1
+*/
+
+
+// TC= O(N)
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n= nums.size();
+        long long sum= 0, sumSq= 0;
+        
+        for(int i=0; i<n; i++)
+        {
+            sum+= nums[i];
+            sumSq+= 1LL* nums[i]* nums[i];
+        }
+
+        long long Sn= (long long) n* (n+1)/2;
+        long long Sn2= (long long) n* (n+1)* (2*n+1)/6;
+
+        long long val1= sum-Sn;     // x-y
+        long long val2= sumSq-Sn2;  // x^2 - y^2
+
+        long long x= val2/val1;
+        x+= val1;
+        x= x/2;
+
+        long long y= x - val1;
+
+       return {(int) x, (int) y};
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Using unordered_map
 // TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -27,3 +79,5 @@ public:
     return {repeat, missing};
     }
 };
+
+*/
