4d4990c8a3aed67239c851863b8c24ea78991166
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-04-23 16:21:46 +0530

    Time: 1 ms (51.86%) | Memory: 7.9 MB (74.92%) - LeetSync

--- 1500-count-largest-group/count-largest-group.cpp
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -1,5 +1,52 @@
-// Approach 1 -> 
+// Approach 2 -> Using separate function to find sum using divide and modulus
+// TC= O(N* log(D))
+
+class Solution{
+ public:
+    int digitSum(int number)
+    {
+        int sum= 0;
+        while(number != 0)
+        {
+            sum+= number % 10;
+            number= number/10;
+        }
+      return sum;  
+    }
+
+    int countLargestGroup(int n)
+    { 
+        // max n can go upto 10^4 -> max sum of digits of 9999 can be 36
+        vector<int> freq(37,0);
+         
+        int maxSize= 0, cnt= 0;
+        for(int i=1; i<=n; i++)
+        {
+            int sum= digitSum(i);
+            freq[sum]++;
+
+            if(freq[sum] > maxSize)
+            {
+                maxSize= freq[sum];
+                cnt= 1;
+            }
+            else if(freq[sum]== maxSize)
+            {
+                cnt++;
+            }
+        }
+     return cnt;   
+    }
+};
+
+
+
+
+
+// Approach 1 -> Iterating on entire digits to find sum
 // TC= O(N*d) + O(N)
+
+/*
 
 class Solution {
 public:
@@ -37,3 +84,5 @@ public:
         return cnt;
     }
 };
+
+*/
