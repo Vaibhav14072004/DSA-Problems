a7affddc5e4c000c5fceb77763043486c2348212
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-05-24 08:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (68.03%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -1,10 +1,25 @@
+// Sum of 2 sides must be > third side
+
 class Solution {
 public:
     string triangleType(vector<int>& nums) {
-        int a = nums[0], b = nums[1], c = nums[2];
-        if(a+b <= c or a+c <= b or b+c <= a) return "none";
-        if(a == b and b == c) return "equilateral";
-        if(a==b or b==c or a == c) return "isosceles";
-        return "scalene";
+        int n= nums.size();
+        int a= nums[0];
+        int b= nums[1];
+        int c= nums[2];
+
+        if(a+b <= c || a+c <= b || b+c <= a)
+        {
+            return "none";
+        }
+        else if(a== b && b== c)
+        {
+            return "equilateral";
+        }
+        else if(a== b || b== c || a== c)
+        {
+            return "isosceles";
+        }
+     return "scalene";   
     }
 };
