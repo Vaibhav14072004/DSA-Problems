76e4601248acf796098162f534f011c2b0dfaf8b
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-03 16:23:12 +0530

    Time: 26 ms (12.98%) | Memory: 45.5 MB (15.38%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -1,5 +1,73 @@
+// Approach 2 -> Using BFS
+// TC= O(N) 
+
+class Solution{
+  public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) { 
+
+       unordered_set<int> visited;
+       unordered_set<int> foundSet;
+       int cnt= 0;
+
+       queue<int> q;
+       for(int &currBox: initialBoxes)
+       {
+           if(status[currBox]== 1){
+               q.push(currBox);
+           }
+
+           // if status[currBox]== 0, means its key is not known
+           else{
+              foundSet.insert(currBox);
+           }
+       }
+
+       while(!q.empty())
+       {
+           int currBox= q.front();
+           q.pop();
+
+           if(visited.count(currBox)){
+              continue;
+           }
+           
+           visited.insert(currBox);
+           cnt+= candies[currBox];
+
+           for(auto &it: containedBoxes[currBox])
+           {
+               if(!visited.count(it) && status[it]== 1)
+               {
+                   q.push(it);
+               }
+               else
+               {
+                  foundSet.insert(it);
+               }
+           }
+
+           for(auto &it: keys[currBox])
+           {
+                status[it]= 1;
+               if(!visited.count(it) && foundSet.count(it))
+               {
+                  q.push(it);
+               }
+           }
+       }
+     return cnt;  
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Using DFS
 // TC= O(N) -> Visited all boxes only at 1 time
+
+/*
 
 class Solution {
 public:
@@ -54,3 +122,5 @@ public:
        return cnt; 
     }
 };
+
+*/
