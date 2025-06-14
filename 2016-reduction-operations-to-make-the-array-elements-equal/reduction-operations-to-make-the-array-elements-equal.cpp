962383f537cab23a7a0959095f0e6a53fdb2bd94
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-14 20:50:58 +0530

    Time: 103 ms (29.92%) | Memory: 95.8 MB (36.61%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,5 +1,49 @@
+// Approach 2 -> Using maxHeap
+// TC= O(N* log N)
+
+class Solution{
+  public:
+    int reductionOperations(vector<int>& nums) {
+        int n= nums.size();
+        priority_queue<int> maxHeap;
+
+        for(int i=0; i<n; i++){
+            maxHeap.push(nums[i]);
+        }
+
+        int ans= 0;
+        int freq= 0;
+        while(maxHeap.size() >= 2)
+        {
+            int first= maxHeap.top();
+            freq++;
+            maxHeap.pop();
+
+            while(!maxHeap.empty() && maxHeap.top()== first){
+                freq++;
+                maxHeap.pop();
+            }
+            
+            // we have not initialized freq again to 0, bcoz we have to again convert all greater elements..
+            // For ex- [5 5 3 3 1], we will first convert 5's to 3 then 3's again to 1
+            if(!maxHeap.empty())
+            {
+                ans+= freq;
+            }
+        }
+    return ans;
+    }
+};
+
+
+
+
+
+
 // Approach 1 ->> Using Sorting
 // TC= O(N* log N)
+
+/*
 
 class Solution {
 public:
@@ -21,3 +65,5 @@ public:
     return cnt;
     }
 };
+
+*/
