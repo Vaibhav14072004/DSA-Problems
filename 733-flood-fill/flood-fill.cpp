391efde36cc3c8374b4b0a85f141df9b6d28f3b8
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-26 12:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (18.91%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,5 +1,58 @@
+// Approach 2 -> Using BFS
+// TC= O(N*M)
+  
+class Solution{
+  public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         int n= image.size();
+         int m= image[0].size();
+
+         vector<vector<bool>> visited(n,vector<bool> (m,false));
+         int oldColor= image[sr][sc];
+         
+         image[sr][sc]= color;
+         visited[sr][sc]= true;
+
+         queue<pair<int,int>> q;
+         q.push({sr,sc});
+
+         vector<int> rowDir= {-1,0,1,0};
+         vector<int> colDir= {0,1,0,-1};
+
+         while(!q.empty())
+         {
+            int row= q.front().first;
+            int col= q.front().second;
+            q.pop();
+
+            visited[row][col]= true;
+            image[row][col]= color;
+
+            for(int k=0; k<4; k++)
+            {
+                int newRow= row+ rowDir[k];
+                int newCol= col+ colDir[k];
+
+                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m &&
+                 !visited[newRow][newCol] && image[newRow][newCol]== oldColor)
+                {
+                    q.push({newRow,newCol});
+                }
+            }
+         }
+     return image;  
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Using DFS
 // TC=O(N*M)
+
+/*
 
 class Solution {
 public:
@@ -38,3 +91,5 @@ public:
         return image;
     }
 };
+
+*/
