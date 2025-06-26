abf976ea2d012d8371b05796ba5628a0999ba52a
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-26 13:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (61.90%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,6 +1,58 @@
+// Approach 1.1 -> Using DFS 
+// No need to take visited vector, bcoz we have already changed its color
+
+// TC=O(N*M)
+// SC= O(M*N) for recursion stack space
+
+class Solution {
+public:
+    void DFS(int i,int j,int &oldColor,int &color,vector<vector<int>> &image, vector<int> &rowDir, vector<int> &colDir)
+    {
+        int n= image.size();
+        int m= image[0].size();
+
+        for(int k=0; k<4; k++)
+        {
+            int nRow= i+ rowDir[k];
+            int nCol= j+ colDir[k];
+
+            if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && image[nRow][nCol]== oldColor)
+            {
+                image[nRow][nCol]= color;
+                DFS(nRow,nCol,oldColor,color,image,rowDir,colDir);
+            }
+        }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n= image.size();
+        int m= image[0].size();
+        
+        int oldColor= image[sr][sc];
+        image[sr][sc]= color;
+        
+        // base case for stopping -> infinite recursion...
+        if(oldColor== color) return image;
+
+        vector<int> rowDir= {-1,0,1,0};
+        vector<int> colDir= {0,1,0,-1};
+
+        DFS(sr,sc,oldColor,color,image,rowDir,colDir);
+        return image;
+    }
+};
+
+
+
+
+
+
+
 // Approach 2 -> Using BFS
 // TC= O(N*M)
   
+/*
+
 class Solution{
   public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
@@ -44,6 +96,7 @@ class Solution{
     }
 };
 
+*/
 
 
 
@@ -51,6 +104,7 @@ class Solution{
 
 // Approach 1 -> Using DFS
 // TC=O(N*M)
+// SC= O(M*N) for recursion stack space
 
 /*
 
