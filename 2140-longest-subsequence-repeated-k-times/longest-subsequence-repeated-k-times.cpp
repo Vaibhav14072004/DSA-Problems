6e06905378ffe4bad56e8fc61977f7026704623f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-28 20:47:02 +0530

    Time: 354 ms (52.86%) | Memory: 10.6 MB (96.43%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -12,13 +12,126 @@
 */
 
 
-// Approach 1 -> Generating all possible subsequences
+// Approach 2 -> Better way to erite backtracking function....
+// TC= O(N)* O(N/k)!
+// TC= O(N/k) + O(26)
+
+class Solution{
+ public:
+    bool isSubsequence(string &str,string &curr,int &k)
+    {
+        int n= str.length();
+        int m= curr.length();
+        int i=0, j= 0;
+
+        while(i < n && j < m*k)
+        {
+            if(str[i]== curr[j % m]){
+                j++;
+            }
+          i++;
+        }
+      return (j== m*k);  
+    }
+
+
+    bool backtracking(string &curr,string &result,int len,string &str,vector<int> &freq,int k)
+    {
+       // base case
+       if(curr.length()== len)
+       {
+           if(isSubsequence(str,curr,k))
+           {
+              result= max(result,curr);
+              return true;
+           }
+          return false; 
+       }
+
+       // start making lexographically largest by starting from 'z' to 'a'
+       for(int i=25; i>=0; i--)
+       {
+           if(freq[i]== 0){
+              continue;
+           }
+
+           // DO
+           curr+= i + 'a';
+           freq[i]--;
+
+           // EXPLORE
+           if(backtracking(curr,result,len,str,freq,k))
+           {
+              return true;
+           }
+
+           // UNDO
+           curr.pop_back();
+           freq[i]++;
+       }
+      return false; 
+    }
+
+
+    string longestSubsequenceRepeatedK(string str, int k) {
+        int n= str.length();
+
+        vector<int> freq(26,0);
+        for(int i=0; i<n; i++){
+            freq[str[i]-'a']++;
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            if(freq[i] < k){
+                freq[i]= 0;
+            }
+
+            // this is the required freq for making each sequence
+            else
+            {
+              freq[i]= freq[i]/k;
+            }
+        }
+
+        // Each sequence can be of maxLen= n/k
+        int maxLen= n/k;
+
+        string curr= "";
+        string result= "";
+
+        // instead of making string curr from 0, start making it from maxLen
+        for(int len=maxLen; len>=0; len--)
+        {
+            // first subsequence will be result as it will be of max possible length 
+            //and lexographically largest as we start making string curr starting from 'z' to 'a'
+
+            vector<int> tempFreq= freq;
+            if(backtracking(curr,result,len,str,tempFreq,k))
+            {
+                result= curr;
+                break;
+            }
+        }
+      return result;  
+    }
+};
+
+
+
+
+
+
+
+
+// Approach 1 -> Generating all possible subsequences...
 // TC= ->> (N/k)! for generating sequence of length N/k and O(N) for checking subsequence..
+// SC= O(N/k) for recursive stack space..
 
 // TC= O(N)* O(N/k)!
-// SC= O(26)+ O(N)
+// SC= O(26)+ O(N/k) 
 
-
+/*
 
 class Solution {
 public:
@@ -108,3 +221,5 @@ public:
      return result;
     }
 };
+
+*/
