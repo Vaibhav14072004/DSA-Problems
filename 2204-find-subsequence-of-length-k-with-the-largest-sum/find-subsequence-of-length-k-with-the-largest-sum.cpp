8a7059a2cc58c6164dd93ff769c9ac91d3c51f7e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-06-28 11:08:59 +0530

    Time: 5 ms (11.61%) | Memory: 13.8 MB (61.71%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,4 +1,49 @@
+// Approach 2 -> Using priority_queue (Max heap)
+// TC= O(N* log N)+ O(k)+ O(K* log K)+ O(N* log N)+ O(N)
+// TC= O(N*log N)
+
+class Solution{
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        if(k > n) return {};
+
+        priority_queue<pair<int,int>> maxHeap;
+        for(int i=0; i<n; i++)
+        {
+            maxHeap.push({nums[i],i});
+        }
+        
+        vector<int> index;
+        while(k--)
+        {
+            int idx= maxHeap.top().second;
+            maxHeap.pop();
+            index.push_back(idx);
+        }
+
+        sort(index.begin(),index.end());
+
+        vector<int> ans;
+        for(int i=0; i<index.size(); i++)
+        {
+            ans.push_back(nums[index[i]]);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> Using Vector
 // TC= O(2N* log N) + O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -34,3 +79,5 @@ public:
       return ans;  
     }
 };
+
+*/
