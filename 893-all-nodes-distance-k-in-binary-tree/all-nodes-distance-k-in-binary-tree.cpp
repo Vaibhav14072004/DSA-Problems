30d1cd79dbc6d5c2a99c2f65f99e5778fe8632a9
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-07-01 19:32:55 +0530

    Time: 8 ms (24.88%) | Memory: 17.5 MB (5.60%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -8,9 +8,106 @@
  * };
  */
 
-// Approach 1 -> Using BFS
-// TC= O(N)
-// SC= O(N)
+
+// Approach 1.2 ->> Using BFS
+// Creating adj list of each node instead of storing parent (using recursion)
+// then traversing each node level wise using queue
+
+// TC= O(3*N)
+// SC= O(2*N), N for recursive stack space, N for queue
+
+class Solution{
+  public:
+    void createAdj(TreeNode* root, TreeNode* parent, unordered_map<TreeNode*,vector<TreeNode*>> &adj)
+    {
+        // base case
+        if(!root) return;
+
+        if(parent != NULL){
+            adj[root].push_back(parent);
+        }
+
+        if(root->left){
+            adj[root].push_back(root->left);
+        }
+
+        if(root->right){
+            adj[root].push_back(root->right);
+        }
+
+        createAdj(root->left,root,adj);
+        createAdj(root->right,root,adj);
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k)
+    {
+       if(root== NULL){
+          return {};
+       }   
+       
+       // first create adj list using recursion..
+       unordered_map<TreeNode*,vector<TreeNode*>> adj;
+       TreeNode* parent= NULL;
+       createAdj(root,parent,adj);
+
+       queue<TreeNode*> q;
+       unordered_set<TreeNode*> visited;
+
+       q.push(target);
+       visited.insert(target);
+
+       // now traverse nodes level wise using queue..
+       int level= 0;
+       while(!q.empty())
+       {
+           int n= q.size();
+
+           // all nodes at level k will be in queue
+           if(level== k)
+           {
+              break;
+           }
+
+           while(n--)
+           {
+               TreeNode* curr= q.front();
+               q.pop();
+               for(auto it: adj[curr])
+               {
+                 if(!visited.count(it)){
+                    q.push(it);
+                    visited.insert(it);
+                  }
+                }
+           }
+        level++;   
+       }
+    
+    // queue will be storing all nodes at level k
+    vector<int> ans;
+
+    while(!q.empty()){
+       ans.push_back(q.front()->val);
+       q.pop();
+    }
+   return ans;
+  }
+};
+
+
+
+
+
+
+
+// Approach 1.1 -> Using BFS
+// Storing parent of each node using queue
+// then traversing each node level wise using queue
+
+// TC= O(3*N)
+// SC= O(2*N)
+
+/*
 
 class Solution {
 public:
@@ -96,3 +193,5 @@ public:
     return ans;
     }
 };
+
+*/
