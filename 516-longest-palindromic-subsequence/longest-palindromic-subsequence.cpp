6f2e7fa7c4ae8293e96f37c91ee9e33c359f51fc
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-07-08 22:56:24 +0530

    Time: 79 ms (56.55%) | Memory: 75.8 MB (73.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,5 +1,48 @@
+// Approach 2 -> Bottom Up
+// Just reversed Top Down Approach..
+// TC= O(N*M)
+
+class Solution{
+ public:
+    int longestPalindromeSubseq(string str) {
+        int n= str.length();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=0; j< n; j++)
+            {
+                if(i > j)
+                {
+                    dp[i][j]= 0;
+                    continue;
+                }
+                if(i== j)
+                {
+                    dp[i][j]= 1;
+                    continue;
+                }
+
+                if(str[i]== str[j])
+                {
+                    dp[i][j]= 2+ dp[i+1][j-1];
+                }
+                else
+                {
+                    dp[i][j]= max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+      return dp[0][n-1];  
+    } 
+};
+
+
+
 // Approach 1 -> Top Down (Memoization)
 // TC= O(N*M)
+
+/*
 
 class Solution {
 public:
@@ -35,3 +78,5 @@ public:
         return solve(0,n-1,str,dp);
     }
 };
+
+*/
