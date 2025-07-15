8a44168807d578a057b6f3f37eb7ea2eb38a86fe
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-07-15 08:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.48%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -8,6 +8,48 @@ public:
         int vowel= 0, consonant= 0, chars= 0;
         for(int i=0; i<n; i++)
         {
+            if((word[i] >= 'A' && word[i] <= 'Z') || word[i] >= 'a' && word[i] <= 'z')
+            {
+               if(word[i]== 'a' || word[i]== 'e' || word[i]== 'i' || word[i]=='o' || word[i]=='u' ||     word[i]=='A' || word[i]=='E' || word[i]=='I' || word[i]=='O' || word[i]=='U'){
+                   vowel++; 
+               }
+               else{
+                consonant++;
+               }
+               chars++;
+            }
+
+            // ascii value of digits is 48 to 57
+            else if(word[i] >= '0' && word[i] <= '9')
+            {
+                chars++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+      return vowel >= 1 && consonant >= 1 && chars >= 3;  
+    }
+};
+
+
+
+
+
+
+/*
+
+class Solution {
+public:
+    bool isValid(string word)
+    {
+        int n= word.length();
+        if(n < 3) return false;
+        
+        int vowel= 0, consonant= 0, chars= 0;
+        for(int i=0; i<n; i++)
+        {
             int ascii= word[i];
             if((ascii >= 65 && ascii <= 90) || ascii >= 97 && ascii <= 122)
             {
@@ -33,3 +75,5 @@ public:
       return vowel >= 1 && consonant >= 1 && chars >= 3;  
     }
 };
+
+*/
