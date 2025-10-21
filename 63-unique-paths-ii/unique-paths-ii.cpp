4f0a5ac572d8cd9d654e39c8deee882db262e8c5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-10-21 17:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (72.90%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,6 +1,55 @@
+// Approach 2 -> Bottom Up (Tabulation)
+// TC= O(N*M)
+// Sc= O(N*M)
+
+class Solution{
+ public: 
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m= grid[0].size();
+
+        vector<vector<long long>> dp(n,vector<long long> (m,0));
+
+        // base case -> if both src and dest have obstacle..
+        if(grid[n-1][m-1]== 1 || grid[0][0]== 1) return 0;
+
+        dp[n-1][m-1]= 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=m-1; j>=0; j--)
+            {
+                if(i== n-1 && j== m-1){
+                    continue;
+                }
+                
+                // if there is obstacle...
+                if(grid[i][j]== 1)
+                {
+                    dp[i][j]= 0;
+                    continue;
+                }
+
+                // move down..
+               if(i+1 < n) dp[i][j]+= dp[i+1][j];
+
+               // move right..
+               if(j+1< m) dp[i][j]+= dp[i][j+1];
+            }
+        }
+    return (int) dp[0][0];
+    }
+};
+
+
+
+
+
+
 // Approach 1 -> Top Down (Memoiztion)
 // TC= O(N*M)
 // SC= O(N*M)
+
+/*
 
 class Solution {
 public:
@@ -46,3 +95,5 @@ public:
         return solve(0,0,grid,dp);
     }
 };
+
+*/
