9b8060d5f8696727b2b88d8ceac91a3178ba66a0
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-10-29 15:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (60.80%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,4 +1,30 @@
+// Approach 2 -> 
+// (2^N - 1) will have all have set bits
+// TC= O(log N)
+
+// 1 3 7 15
+
+class Solution{
+  public:
+    int smallestNumber(int n) 
+    {
+        int result= 1;
+        while(result < n)
+        {
+            result= 2*result+1;
+        }
+       return result; 
+    }
+};
+
+
+
+
+
 // Approach 1 -> Starting from n+1, check if all the bits are set or NOT
+// TC= O(No of bits) => O(log n)
+
+/*
 
 class Solution {
 public:
@@ -15,3 +41,5 @@ public:
       return n;  
     }
 };
+
+*/
