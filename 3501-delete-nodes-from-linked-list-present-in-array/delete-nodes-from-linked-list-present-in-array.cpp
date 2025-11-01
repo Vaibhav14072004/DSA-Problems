35ca1a33231fdc4e75fd729d95560e60c52f2c33
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-11-01 22:03:15 +0530

    Time: 99 ms (59.60%) | Memory: 263.3 MB (76.09%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,48 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-// TC= O(N + N) -> O(N)
-
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        
-        ListNode* dummy= new ListNode(-1);
-        dummy->next= head;
-
-        ListNode* curr= head;
-        ListNode* prev= dummy;
-
         unordered_set<int> st(nums.begin(), nums.end());
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
 
-        while(curr != NULL)
-        {
-            // delete curr node if it is present in the set
-            if(st.find(curr->val) != st.end())
-            {
-                prev->next= curr->next;
-                ListNode* temp= curr;
-                curr= curr->next;
-                delete temp;
-            }
+        ListNode* curr = head;
+        ListNode* prev = dummy;
 
-            else
-            {
-                prev= curr;
-                curr= curr->next;
+        while (curr != NULL) {
+            if (st.find(curr->val) != st.end()) {
+                prev->next = curr->next;
+                curr = curr->next;  // just move, don’t delete
+            } else {
+                prev = curr;
+                curr = curr->next;
             }
         }
 
-       ListNode* newHead= dummy->next;
-       delete dummy;
-       return newHead;
+        ListNode* newHead = dummy->next;
+        delete dummy; // safe to delete dummy
+        return newHead;
     }
 };
