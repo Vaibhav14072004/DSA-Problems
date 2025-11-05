f6eb5c9a8452ec72eaac5b8722a5881723fee2d6
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-11-05 12:36:15 +0530

    Time: 769 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,14 @@
-# Write your MySQL query statement below
+# Approach 2 ->
 
-  SELECT DISTINCT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary NOT in
-  ( SELECT DISTINCT MAX(salary) FROM Employee);
+SELECT(
+SELECT DISTINCT e1.salary FROM Employee e1 WHERE 1= (
+    SELECT COUNT(DISTINCT salary) FROM Employee e2 WHERE e1.salary < e2.salary)
+) AS SecondHighestSalary;
+
+
+# Approach 1 -> 
+
+/*
+SELECT DISTINCT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary NOT in  
+( SELECT DISTINCT MAX(salary) FROM Employee);
+*/
