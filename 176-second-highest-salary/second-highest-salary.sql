e8a85d036a4c710d8d6acccadcf7101ccdcb54ae
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-11-05 12:59:07 +0530

    Time: 277 ms (32.45%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,18 @@
+# Approach 3 ->
+
+SELECT(
+ SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET 1
+) as SecondHighestSalary;
+
+
 # Approach 2 ->
 
+/*
 SELECT(
 SELECT DISTINCT e1.salary FROM Employee e1 WHERE 1= (
     SELECT COUNT(DISTINCT salary) FROM Employee e2 WHERE e1.salary < e2.salary)
 ) AS SecondHighestSalary;
+*/
 
 
 # Approach 1 -> 
