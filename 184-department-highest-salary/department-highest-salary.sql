9782fb80ccea114cd789ebe56d9924442ebf471e
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-11-06 15:31:53 +0530

    Time: 727 ms (45.71%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,6 +1,20 @@
+# approach 1 -> window function (dense rank)
 
+/*
 select Department, Employee, Salary from
 (select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() OVER (
 PARTITION BY d.name ORDER BY e.salary desc) AS drank
 from Employee e JOIN Department d on e.departmentId= d.id) t
 where t.drank= 1 
+*/
+
+
+# approach 2 -> window func (dense rank)
+
+WITH CTE AS (
+    select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() 
+    OVER (PARTITION BY d.name ORDER BY e.salary desc) AS drank
+    FROM Employee e JOIN Department d on e.departmentId= d.id
+) 
+
+SELECT Department,Employee,Salary from CTE where drank= 1;
