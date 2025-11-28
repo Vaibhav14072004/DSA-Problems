9a9db5463247187f5fc3c59331bc040ba35f1f7f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-11-28 17:05:48 +0530

    Time: 218 ms (13.17%) | Memory: 208.8 MB (7.47%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,99 +1,99 @@
-// Approach 2-> DFS using visited vector
-
-class Solution{
+//MERRY CHRISTMAS!! 25-DEC-2024 ~Vipul
+class Solution {
 public:
-    int dfs(int currNode, vector<vector<int>> &adjList, vector<bool> &visited,vector<int> &values,int &k, long long &sum)
+    //METHOD-2
+    //DFS (RECURSION...)
+    int ans=0;
+    long long dfs(int node,unordered_map<int,vector<int>>&mp,vector<int>&values,vector<int>&vis,int k)
     {
-        visited[currNode]= true;
-        sum+= values[currNode];
-        int cnt= 0;
-   
-        for(auto neighbour: adjList[currNode])
+        vis[node]= 1;
+        long long sum= values[node];
+        for(auto i:mp[node])
         {
-            if(!visited[neighbour])
+            if(vis[i]==1)
             {
-                long long childSum= 0;
-                cnt+= dfs(neighbour, adjList, visited,values,k, childSum);
-                sum+= childSum;
+                continue;
+            }
+            sum+= dfs(i,mp,values,vis,k);
+            sum%=k;//to avoid out of range for integer..
+        }
+        if(sum%k==0)
+        {
+            ans++;
+            return 0;
+        }
+        return sum;
+    }
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
+    {
+        if(n==1)
+        {
+            return (values[0]%k==0)? 1:0;
+        }
+        unordered_map<int,vector<int>>mp;
+        vector<int>vis(n,0);
+        for(auto i:edges)
+        {
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+        }
+        //assuming start node is 0
+        dfs(0,mp,values,vis,k);
+        return ans;
+    }
+};
+/*
+//METHOD-1
+//BFS (QUEUE...)
+int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
+    {
+        //base case when edges=[]
+        if(n==1)
+        {
+            return (values[0]%k==0)? 1:0;
+        }
+        unordered_map<int,vector<int>>mp;
+        vector<int>indegree(n,0);
+        for(auto i:edges)
+        {
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
+            indegree[i[1]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            //pushing only leaf nodes..
+            if(indegree[i]==1)
+            {
+                q.push(i);
             }
         }
-
-        if(sum % k==0)
+        int cnt=0;
+        while(q.size())
         {
-            cnt++;
-            sum= 0;
+            int temp= q.front();
+            q.pop();
+            indegree[temp]--;
+            long long toadd= (values[temp]%k==0) ? 0:values[temp]%k; //to avoid integer overflow we will do values[temp]%k
+            if(values[temp]%k==0)
+            {
+                cnt++;
+            }
+            for(auto i:mp[temp])
+            {
+                if(indegree[i]>0)
+                {
+                    indegree[i]--;
+                    values[i]+= toadd;
+                    if(indegree[i]==1)
+                    {
+                        q.push(i);
+                    }
+                }
+            }
         }
         return cnt;
     }
-
-
-
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        // create adjList
-        vector<vector<int>> adjList(n);
-
-        for(auto it: edges)
-        {
-            adjList[it[0]].push_back(it[1]);
-            adjList[it[1]].push_back(it[0]);
-        }
-
-        vector<bool> visited(n,false);
-        long long sum= 0;
-
-        return dfs(0,adjList,visited,values,k,sum);
-    }
-};
-
-
-
-
-
-
-// Approach 1 -> DFS without using visited vector
-// TC= O(N)
-
-/*
-
-class Solution {
-public:
-    int dfs(int node, int parent,vector<vector<int>> &adjList,vector<int> &values, int &k, long long &sum)
-    {
-         sum+= values[node];
-         int cnt= 0;
-
-         for(int neighbour: adjList[node])
-         {
-            if(neighbour != parent)
-            {
-                long long childSum= 0;
-               cnt+= dfs(neighbour, node, adjList, values, k, childSum);
-
-               sum+= childSum;
-            }  
-         }
-
-         if(sum % k== 0)
-         {
-            cnt++;
-            sum= 0;
-         }
-
-         return cnt;
-    }
-
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        // create adjList
-        vector<vector<int>> adjList(n);
-        for(auto it: edges)
-        {
-            adjList[it[0]].push_back(it[1]);
-            adjList[it[1]].push_back(it[0]);
-        }
-        
-        long long sum= 0;
-        return dfs(0,-1,adjList,values,k,sum);
-    }
-};
-
 */
