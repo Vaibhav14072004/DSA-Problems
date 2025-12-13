9e0e56ac303519e8c6a7db34e1c81e39cace05d5
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-12-13 23:01:59 +0530

    Time: 23 ms (32.85%) | Memory: 27 MB (26.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,6 +1,47 @@
+// Approach 2 -> Using 26 size freq (WITHOUT Sorting)
+// TC= O(N* (M+26))
 
-// Approach 1 -> Using unordered_map
+class Solution{
+  public: 
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+
+        for(string it: strs)
+        {
+            vector<char> temp(26,0);
+            for(char ch: it)
+            {
+                temp[ch-'a']++;
+            }
+
+            string sortedString= "";
+            for(int i=0; i<26; i++)
+            {
+                while(temp[i]-- > 0) sortedString+= (i+'a');
+            }
+
+            mp[sortedString].push_back(it);
+        }
+
+        for(auto it: mp)
+        {
+            ans.push_back(it.second);
+        }
+      return ans;  
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> Using unordered_map + Sorting
 // TC= O[N* (m* log m)]
+
+/*
 
 class Solution {
 public:
@@ -22,3 +63,5 @@ public:
      return ans;  
     }
 };
+
+*/
