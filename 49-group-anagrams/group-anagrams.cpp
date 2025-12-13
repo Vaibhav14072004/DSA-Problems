7a0268562e8164ae071ce6fbd28fd3d0c868d266
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-12-13 21:03:51 +0530

    Time: 15 ms (70.47%) | Memory: 26.1 MB (46.69%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,24 @@
+
+// Approach 1 -> Using unordered_map
+// TC= O[N* (m* log m)]
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        
+        unordered_map<string,vector<string>> mp;
+        for(auto it: strs)
+        {
+            string temp= it;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(it);
+        }
+
+       for(auto it: mp)
+       {
+           ans.push_back(it.second);
+       } 
+     return ans;  
+    }
+};
