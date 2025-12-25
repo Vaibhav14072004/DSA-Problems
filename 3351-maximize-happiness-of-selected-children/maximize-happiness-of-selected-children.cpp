78932006b30f6234bc6fa327316547cda08afb94
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2025-12-25 10:03:14 +0530

    Time: 183 ms (28.01%) | Memory: 111.9 MB (12.77%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,5 +1,35 @@
+// Approach 2 -> Using priority_heap
+// TC= O(N* log N)+ O(N* log N)
+
+class Solution{
+ public:
+   long long maximumHappinessSum(vector<int> &happiness, int k)
+   {
+       int n= happiness.size();
+       long long ans= 0;
+       priority_queue<int> maxHeap(happiness.begin(),happiness.end());
+
+
+        int cnt= 0;
+       while(!maxHeap.empty() && (k--))
+       {
+           int temp= maxHeap.top();
+           maxHeap.pop();
+           temp-= cnt;
+           if(temp > 0) ans+= temp;
+           cnt++;
+       }
+     return ans;  
+   }
+};
+
+
+
+
 // Approach 1 -> Sorting
 // TC= O(N* log N)+ O(N)
+
+/*
 
 class Solution {
 public:
@@ -19,3 +49,5 @@ public:
       return ans;  
     }
 };
+
+*/
