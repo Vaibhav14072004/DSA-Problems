0012abe950936e1215e1f2c64d7e7be08d5e4e3f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-02-07 07:43:35 +0530

    Time: 37 ms (34.91%) | Memory: 55 MB (20.86%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,3 +1,46 @@
+// Approach 2 ->> Using 2 vectors...
+
+// Find min of all [(No of b on left side) + (No of a on right side)] for each index
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+
+        vector<int> leftB(n, 0);
+        leftB[0] = (s[0] == 'b');
+
+        for (int i = 1; i < n; i++) {
+            leftB[i] = leftB[i - 1] + (s[i] == 'b');
+        }
+
+        vector<int> rightA(n, 0);
+        rightA[n - 1] = (s[n - 1] == 'a');
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightA[i] = rightA[i + 1] + (s[i] == 'a');
+        }
+
+        int ans = min(leftB[n - 1], rightA[0]); // all b’s or all a’s
+
+        for (int j = 1; j < n; j++) {
+            ans = min(ans, leftB[j - 1] + rightA[j]);
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+// Approach 1 -> Stack
+// TC= O(N)    SC= O(1)
+
+/*
+
 class Solution {
 public:
     int minimumDeletions(string s) {
@@ -12,7 +55,8 @@ public:
                 st.push(ch);
             }
         }
-
         return count;
     }
 };
+
+*/
