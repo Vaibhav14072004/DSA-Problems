fef5a50b2e83fd26044c0df6e41e7fbedea6601d
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-03-08 12:47:31 +0530

    Time: 1 ms (27.83%) | Memory: 14.7 MB (14.10%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,74 +1,36 @@
-// Approach 2 -> BackTracking...
-// TC= O(2^N)
-// SC= O(N^2) + O(N) for recursive stack space
-
-class Solution{
-public:
-    void solve(vector<string> &nums, string &curr, string &ans,unordered_set<string> &st)
-    {
-        // base case
-        if(!ans.empty()) return;
-
-        if(curr.size()== nums.size())
-        {
-            if(!st.count(curr))
-            {
-                ans= curr;
-            }
-           return; 
-        }
-        
-        // First try to append '0' and call recursion
-        curr+= '0';
-        solve(nums,curr,ans,st);
-        curr.pop_back();
-        
-        // Then try to append '1' and call recursion
-        curr+= '1';
-        solve(nums,curr,ans,st);
-        curr.pop_back();
-    }
-
-    string findDifferentBinaryString(vector<string>& nums) {
-        int n= nums.size();
-        string ans= "";
-        string curr= "";
-        unordered_set<string> st(nums.begin(),nums.end());
-        solve(nums,curr,ans,st);
-        return ans;
-    }
-};
-
-
-
-
-
-
-
-// Optimized Best Approach 1 ->> Just put the opposite of every digit in ans
-// TC= O(N)
-
-/*
-
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        int n= nums.size();
-        string ans= "";
-        
-        for(int i=0;i<n;i++)
+    void solve(vector<string>& nums,string curr,string &res,unordered_set<string>&st,int &n)
+    {
+        if(res!="")
         {
-            if(nums[i][i]== '0')
-            {
-                ans+= '1';
-            }
-            else
-            {
-                ans+= '0';
-            }
+            return;
         }
-    return ans;
+        if(curr.size()>n)
+        {
+            return;
+        }
+        if(curr.size()==n && st.find(curr)==st.end())
+        {
+            res= curr;
+            st.insert(curr);
+            return;
+        }
+        curr.push_back('0');
+        solve(nums,curr,res,st,n);
+        curr.pop_back();
+
+        curr.push_back('1');
+        solve(nums,curr,res,st,n);
+        curr.pop_back();
+    }
+    string findDifferentBinaryString(vector<string>& nums) 
+    {
+        unordered_set<string>st(begin(nums),end(nums));
+        string res="";
+        string curr="";
+        int n= nums[0].size();
+        solve(nums,curr,res,st,n);
+        return res;
     }
 };
-
-*/
