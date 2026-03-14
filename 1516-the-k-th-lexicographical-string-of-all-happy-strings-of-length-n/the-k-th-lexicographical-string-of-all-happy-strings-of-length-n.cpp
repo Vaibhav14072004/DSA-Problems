196f1d5d5a30f4aa6159a73ddade9b5a386d7c19
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-03-14 08:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (80.34%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,100 +1,30 @@
-// Ques of Backtracking... (DO, EXPLORE, UNDO)
-
-// Approach 2 ->> Using cnt variable...
-// TC= O[3* 2^(N-1)]
-// SC= O(N) for recursive stack space
-
-class Solution{
-  public:
-    void solve(int &cnt,string &ans,string &curr,int n, int k)
-    {
-        // base case
-        if(curr.size()== n)
-        {
-            cnt++;
-            if(cnt== k)
-            {
-                ans= curr;
-            }
-            return;
-        }
-
-        for(char ch= 'a'; ch<= 'c'; ch++)
-        {
-            // adjacent chars should NOT be equal..
-            if(!curr.empty() && curr.back()== ch){
-                continue;
-            }
-
-            curr+= ch;
-            solve(cnt,ans,curr,n,k);
-            
-            // backtracking...
-            curr.pop_back();
-        }
-    }
-
-    string getHappyString(int n, int k){
-        int cnt= 0;
-        string ans= "";
-        string curr= "";
-        solve(cnt,ans,curr,n,k);
-        return ans;  
-    }
-};
-
-
-
-
-
-
-
-
-
-// Approach 1 -> Storing all strings ..
-
-// TC= O[3* 2*2*2..... upto n-1] ->> 3* 2^(N-1)
-// SC= O(N) for recursive stack + O[N* 2^N] bcoz there will be 2^N strings in ans vector and each string is of size n
-
-// TC= O[1.5* 2^N] ->> O(2^N)
-// SC= O(N* 2^N)
-
-/*
-
 class Solution {
 public:
-    void solve(vector<string> &ans,string &curr, int n, int k)
-    {
-        // base case
-        if(curr.size()== n){
-            ans.push_back(curr);
-            return;
+    string getHappyString(int n, int k) {
+        int perCharCount=pow(2,n-1);
+        if(3*perCharCount<k)return "";
+        string ans="";
+        if(k<=perCharCount){
+            ans.push_back('a');
+        }else if(k<=2*perCharCount){
+            ans.push_back('b');
+            k-=perCharCount;
+        }else{
+            ans.push_back('c');
+            k-=2*perCharCount;
         }
-
-        for(char ch= 'a'; ch<= 'c'; ch++)
-        {
-            // adjacent chars must be different..
-            if(!curr.empty() && curr.back()== ch){
-                continue;
+        vector<string>options{"bc","ac","ab"};
+        for(int i=1;i<n;i++){
+            perCharCount/=2;
+            // perCharCount=pow(2,n-i-1);
+            string option=options[ans.back()-'a'];
+            if(k<=perCharCount){
+                ans.push_back(option[0]);
+            }else{
+                ans.push_back(option[1]);
+                k-=perCharCount;
             }
-            curr+= ch;           // DO
-            solve(ans,curr,n,k); // EXPLORE
-
-            // backtracking...  UNDO
-            curr.pop_back();
         }
-    }
-
-    string getHappyString(int n, int k){
-        vector<string> ans;
-        string curr= "";
-        solve(ans,curr,n,k);
-
-        if(ans.size() < k){
-            return "";
-        }
-        return ans[k-1];
+        return ans;
     }
 };
-
-*/
