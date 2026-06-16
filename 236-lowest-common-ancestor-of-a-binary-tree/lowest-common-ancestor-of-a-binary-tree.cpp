83362b17a56557be6baf262d04a983f5e4cce8bf
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-06-16 23:12:43 +0530

    Time: 60 ms (18.54%) | Memory: 44 MB (5.92%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,85 +1,34 @@
-// Approach 2 -> get path from (root to p) and (root to q)
-// and then find traverse both paths and find common point...
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
-// TC= O(N)   Sc= O(N)
-
-class Solution{
-public:
-    bool getPath(TreeNode* root, TreeNode* target, vector<TreeNode*> &path)
-    {
-        if(root== NULL){
-            return false;
-        }
-        
-        path.push_back(root);
-
-        if(root== target){
-            return true;
-        }
-
-        if(getPath(root->left,target,path) || getPath(root->right,target,path))
-        {
-            return true;
-        }
-
-        path.pop_back(); // backtracking..
-        return false;
-    }
-
-
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root== NULL){
-            return NULL;
-        }
-
-        vector<TreeNode*> path1, path2;
-         
-        getPath(root, p, path1);
-        getPath(root, q , path2);
-
-        int j= 0;
-        while(j < path1.size() && j < path2.size() && path1[j]== path2[j])
-        {
-            j++;
-        }
-
-       return path1[j-1];
-    }
-};
-
-
-
-
-
-
-// Approach 1 -> Optimal 
-// Tc= O(N)   Sc= O(1)
-
-/*
+// Optimized Approach
+// TC= O(N)
 
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root== NULL){
-            return NULL;
-        }
+        if(root== NULL) return NULL;
         
-        if(root== p || root== q){
+        if(root== p || root== q) return root;
+
+        TreeNode* leftN= lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightN= lowestCommonAncestor(root->right,p,q);
+
+        if(leftN != NULL && rightN != NULL)
+        {
             return root;
         }
-
-        TreeNode* leftNode= lowestCommonAncestor(root->left, p, q);
-        TreeNode* rightNode= lowestCommonAncestor(root->right, p, q);
-
-        if(leftNode && rightNode){
-            return root;
+        else if(leftN != NULL)
+        {
+            return leftN;
         }
-
-        if(leftNode){
-            return leftNode;
-        }
-        return rightNode;
+        return rightN;
     }
 };
-
-*/
