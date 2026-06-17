213f0c14213bb3d5a458e1c5b5da378fb71435ba
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-06-17 17:51:58 +0530

    Time: 65 ms (14.30%) | Memory: 49.5 MB (6.62%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,8 +8,57 @@
  * };
  */
 
-// Optimized Approach
+
+
+// Brute Force Approach
+// Storing all possible ancestors for both nodes and then comparing it 
+
 // TC= O(N)
+// SC= O(H) where H -> height of binary tree
+
+bool getPath(TreeNode* root,TreeNode* targetNode, vector<TreeNode*> &path)
+{
+    if(root== NULL) return false;
+
+    path.push_back(root);
+    
+    if(root==  targetNode) return true;
+
+    if(getPath(root->left,targetNode,path) || getPath(root->right,targetNode,path)){
+        return true;
+    }
+
+    // backtarcking..
+    path.pop_back();
+    return false;
+}
+
+class Solution{
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root== NULL) return NULL;  
+        
+        vector<TreeNode*> path1,path2;
+        getPath(root,p,path1);
+        getPath(root,q,path2);
+
+        int i=0;
+        while(i< path1.size() && i < path2.size() && path1[i]== path2[i])
+        {
+            i++;
+        }
+    return path1[i-1]; 
+    }
+};
+
+
+
+
+
+// Optimized Approach ->> Recursion..
+// TC= O(N)
+
+/*
 
 class Solution {
 public:
@@ -32,3 +81,5 @@ public:
         return rightN;
     }
 };
+
+*/
