b2e6f1bb885e21c241d1e370bf202c0b85d430a0
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-07-10 23:28:10 +0530

    Time: 663 ms (79.08%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,9 +1,20 @@
 # Write your MySQL query statement below
 
-with cte as (
-    select d.name as Department, e.name as Employee, e.salary as Salary,
-     dense_rank() over (partition by d.name order by e.salary desc) as drank
-    from Employee e JOIN Department d on e.departmentId= d.id
-)
+# Approach 1 
 
-select Department,Employee,Salary from cte where drank = 1
+-- with cte as (
+--     select d.name as Department, e.name as Employee, e.salary as Salary,
+--      dense_rank() over (partition by d.name order by e.salary desc) as drank
+--     from Employee e JOIN Department d on e.departmentId= d.id
+-- )
+
+-- select Department,Employee,Salary from cte where drank = 1
+
+
+
+# Approach 2 
+
+Select Department, Employee, Salary from (
+select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank()
+over (partition by d.name order by e.salary desc) as drank from Employee e JOIN 
+Department d on e.departmentId= d.id) t where t.drank = 1
