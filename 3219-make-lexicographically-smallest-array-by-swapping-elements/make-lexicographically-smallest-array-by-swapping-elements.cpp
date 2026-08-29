c6590e55f12addc818e6edc7cee930f75a85dc5f
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2026-08-29 08:29:16 +0530

    Time: 335 ms (38.31%) | Memory: 221.8 MB (31.83%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,24 +1,68 @@
-#define pi pair<int,int>
+
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        priority_queue<pi, vector<pi>, greater<pi>> minheap;
-        for(int i = 0;i<nums.size();i++) minheap.push({nums[i],i});
+        int n = nums.size();
 
-        while(!minheap.empty()){
-            pi temp = minheap.top();
-            multiset<int> value, index;
-            while(!minheap.empty() && temp.first + limit >= minheap.top().first){
-                pi t = minheap.top(); minheap.pop();
-                value.insert(t.first); index.insert(t.second);
-                temp = t;
+        // Copy nums and sort it.
+        // We use this to find connected groups.
+        vector<int> Groups = nums;
+        sort(Groups.begin(), Groups.end());
+
+        // Stores all sorted values belonging to each group.
+        unordered_map<int, vector<int>> Group;
+
+        // Stores which group a particular value belongs to.
+        unordered_map<int, int> Belong;
+
+        // Points to the smallest unused value in each group.
+        unordered_map<int, int> Minindex;
+
+        int id = 1;
+
+        // First value belongs to the first group.
+        Group[id].push_back(Groups[0]);
+
+        // Create groups.
+        // If consecutive sorted values differ by more than limit,
+        // they cannot belong to the same connected component.
+        for (int i = 1; i < n; i++) {
+            if (Groups[i] - Groups[i - 1] > limit) {
+                id++;
             }
 
-            auto it1 = index.begin();  auto it2 = value.begin();
-            while(it1 != index.end()){
-                nums[*it1] = *it2;
-                it1++; it2++;
+            Group[id].push_back(Groups[i]);
+        }
+
+        // Find the group ID for every value.
+        id = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && Groups[i] - Groups[i - 1] > limit) {
+                id++;
             }
-        }return nums;
+
+            Belong[Groups[i]] = id;
+        }
+
+        // Initially, no value from any group has been used.
+        for (int i = 1; i <= id; i++) {
+            Minindex[i] = 0;
+        }
+
+        // Traverse the original array from left to right.
+        // At every position, place the smallest unused value
+        // from the same group to make the array lexicographically smallest.
+        for (int i = 0; i < n; i++) {
+            int groupId = Belong[nums[i]];
+
+            // Take the smallest unused value from this group.
+            nums[i] = Group[groupId][Minindex[groupId]];
+
+            // Move pointer to the next available value.
+            Minindex[groupId]++;
+        }
+
+        return nums;
     }
 };
